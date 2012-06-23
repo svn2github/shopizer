1ebb8717bc29cb3f224872084bee3e5efed0b966
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2012-06-23 11:47:11 +0000

    filter availabilities
    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@588 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/business/catalog/common/CatalogServiceHelper.java
+++ sm-core/src/main/java/com/salesmanager/core/business/catalog/common/CatalogServiceHelper.java
@@ -1,6 +1,8 @@
 package com.salesmanager.core.business.catalog.common;
 
 import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 import com.salesmanager.core.business.catalog.product.model.Product;
@@ -9,6 +11,7 @@ import com.salesmanager.core.business.catalog.product.model.attribute.ProductOpt
 import com.salesmanager.core.business.catalog.product.model.attribute.ProductOptionDescription;
 import com.salesmanager.core.business.catalog.product.model.attribute.ProductOptionValue;
 import com.salesmanager.core.business.catalog.product.model.attribute.ProductOptionValueDescription;
+import com.salesmanager.core.business.catalog.product.model.availability.ProductAvailability;
 
 
 public class CatalogServiceHelper {
@@ -52,6 +55,42 @@ public class CatalogServiceHelper {
 				}
 					
 			}
+		}
+		
+	}
+	
+	/**
+	 * Overwrites the availability in order to return 1 price / region
+	 * @param product
+	 * @param locale
+	 */
+	public static void setToAvailability(Product product, Locale locale) {
+		
+		Set<ProductAvailability> availabilities =  product.getAvailabilities();
+		
+		ProductAvailability defaultAvailability = null;
+		ProductAvailability localeAvailability = null;
+		
+		for(ProductAvailability availability : availabilities) {
+			
+			if(availability.getRegion().equals(ProductAvailability.DEFAULT_AVAILABILITY)) {
+				defaultAvailability = availability;
+			} 
+			if(availability.getRegion().equals(locale.getCountry())) {
+				localeAvailability = availability;
+			}
+			
+		}
+		
+		if(defaultAvailability!=null || localeAvailability!=null) {
+			Set<ProductAvailability> productAvailabilities = new HashSet<ProductAvailability>();
+			if(defaultAvailability!=null) {
+				productAvailabilities.add(defaultAvailability);
+			}
+			if(localeAvailability!=null) {
+				productAvailabilities.add(localeAvailability);
+			}
+			product.setAvailabilities(productAvailabilities);
 		}
 		
 	}
