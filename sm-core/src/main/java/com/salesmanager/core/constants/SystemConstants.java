dc8454016765458bbfbfe991eb429990133e7271
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2012-06-23 11:51:28 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@596 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/constants/SystemConstants.java
+++ sm-core/src/main/java/com/salesmanager/core/constants/SystemConstants.java
@@ -4,7 +4,7 @@ public class SystemConstants {
 	
 	
 
-	
+	public final static String SYSTEM_USER = "SYSTEM";
 	public final static String CONFIG_VALUE_TRUE = "true";
 	public final static String CONFIG_VALUE_FALSE = "false";
 
