ce07e36bf43ea78fb45fb64b044fca17b9f1e13f
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2012-06-27 00:36:50 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@629 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/business/reference/currency/model/Currency.java
+++ sm-core/src/main/java/com/salesmanager/core/business/reference/currency/model/Currency.java
@@ -2,6 +2,7 @@ package com.salesmanager.core.business.reference.currency.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Cacheable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,6 +16,7 @@ import com.salesmanager.core.constants.SchemaConstant;
 
 @Entity
 @Table(name = "CURRENCY", schema = SchemaConstant.SALESMANAGER_SCHEMA)
+@Cacheable
 public class Currency extends SalesManagerEntity<Long, Currency> implements Serializable {
 	private static final long serialVersionUID = -999926410367685145L;
 	
