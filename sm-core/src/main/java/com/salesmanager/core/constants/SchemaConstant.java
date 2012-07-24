a0d9ca1b62e10295ae32c81c2df23337184fac96
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2012-07-24 11:43:07 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@671 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/constants/SchemaConstant.java
+++ sm-core/src/main/java/com/salesmanager/core/constants/SchemaConstant.java
@@ -43,7 +43,7 @@ public class SchemaConstant {
 
 	static {
 		for (Locale locale : Locale.getAvailableLocales()) {
-			LOCALES.put(locale.getISO3Country(), locale);
+			LOCALES.put(locale.getCountry(), locale);
 		}
 	}
 	
