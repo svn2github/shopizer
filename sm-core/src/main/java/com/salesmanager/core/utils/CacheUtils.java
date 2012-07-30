7ae3720440b85708fdf17e0d24ced1fa4cecd8d8
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2012-07-30 11:13:09 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@694 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/utils/CacheUtils.java
+++ sm-core/src/main/java/com/salesmanager/core/utils/CacheUtils.java
@@ -1,12 +1,12 @@
 package com.salesmanager.core.utils;
 
-import net.sf.ehcache.Cache;
-import net.sf.ehcache.CacheManager;
-import net.sf.ehcache.Element;
-
+import org.jboss.cache.CacheFactory;
+import org.jboss.cache.DefaultCacheFactory;
+import org.jboss.cache.Fqn;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@SuppressWarnings("rawtypes")
 public class CacheUtils {
 	
 	
@@ -16,15 +16,19 @@ public class CacheUtils {
 	
 	private static CacheUtils cacheUtils = null;
 
-	private CacheManager singletonManager;
+	private org.jboss.cache.Node refCache;
+	
 	
 	private CacheUtils() {
 		
 		try {
 			
-			singletonManager = CacheManager.getInstance();
-			Cache memoryOnlyCache = new Cache(REFERENCE_CACHE, 0, false, false, 36000, 2);
-			singletonManager.addCache(memoryOnlyCache);
+			   
+			   CacheFactory factory = new DefaultCacheFactory();
+			   org.jboss.cache.Cache cache = factory.createCache();
+			   org.jboss.cache.Node rootNode = cache.getRoot();
+			   Fqn refFqn = Fqn.fromString("/ref/");
+			   refCache = rootNode.addChild(refFqn);
 			
 			
 		} catch (Exception e) {
@@ -46,23 +50,22 @@ public class CacheUtils {
 	
 
 	
-	public void putInCache(Object object, String keyName, String cacheName) throws Exception {
+	@SuppressWarnings("unchecked")
+	public void putInCache(Object object, String keyName) throws Exception {
 		
-		Cache cache = singletonManager.getCache(cacheName);
-		Element element = new Element(keyName, object);
-		cache.put(element);
+		refCache.put("COUNTRIES", object);
+
 		
 	}
 	
-	public Object getFromCache(String keyName, String cacheName) throws Exception {
+	@SuppressWarnings("unchecked")
+	public Object getFromCache(String keyName) throws Exception {
 		
-		Cache cache = singletonManager.getCache(cacheName);
-		Element element = cache.get(keyName);
-		if(element!=null) {
-			return element.getValue();
-		}
+		 return refCache.get("COUNTRIES");
 		
-		return null;
+	}
+	
+	public void shutDownCache() throws Exception {
 		
 	}
 
