d5d1998212819b5204632f0a7850305ba2b69b12
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2012-10-04 22:11:17 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@836 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/modules/cms/product/ProductFileManager.java
+++ sm-core/src/main/java/com/salesmanager/core/modules/cms/product/ProductFileManager.java
@@ -3,7 +3,7 @@ package com.salesmanager.core.modules.cms.product;
 
 
 
-public abstract class ProductFileManager implements ProductImagePut, ProductImageGet, ProductImageRemove {
+public abstract class  ProductFileManager implements ProductImagePut, ProductImageGet, ProductImageRemove {
 	
 	
 
