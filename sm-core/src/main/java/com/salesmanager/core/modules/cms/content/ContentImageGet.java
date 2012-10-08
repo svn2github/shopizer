7a8202885687ff8148a8488df5ed1009a5ad49cf
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2012-10-08 23:06:11 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@855 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/modules/cms/content/ContentImageGet.java
+++ sm-core/src/main/java/com/salesmanager/core/modules/cms/content/ContentImageGet.java
@@ -1,5 +1,8 @@
 package com.salesmanager.core.modules.cms.content;
 
+import java.util.List;
+
+import com.salesmanager.core.business.content.model.image.ImageContentType;
 import com.salesmanager.core.business.content.model.image.OutputContentImage;
 import com.salesmanager.core.business.generic.exception.ServiceException;
 import com.salesmanager.core.business.merchant.model.MerchantStore;
@@ -7,6 +10,7 @@ import com.salesmanager.core.modules.cms.common.ImageGet;
 
 public interface ContentImageGet extends ImageGet {
 	
-	public OutputContentImage getImage(MerchantStore store, String imageName) throws ServiceException;
+	public OutputContentImage getImage(MerchantStore store, String imageName,ImageContentType imageContentType) throws ServiceException;
+	public List<String> getImageNames(MerchantStore store, ImageContentType imageContentType) throws ServiceException;
 
 }
