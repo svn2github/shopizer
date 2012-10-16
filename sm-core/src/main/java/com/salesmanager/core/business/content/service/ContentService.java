bab6b5b95253f3f9af4bb2992f85e226562b0734
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2012-10-16 22:51:18 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@883 e80ee2c7-76f3-4005-aeaf-98396743f455

---
+++ sm-core/src/main/java/com/salesmanager/core/business/content/service/ContentService.java
@@ -0,0 +1,22 @@
+package com.salesmanager.core.business.content.service;
+
+import java.util.List;
+
+import com.salesmanager.core.business.content.model.content.Content;
+import com.salesmanager.core.business.generic.exception.ServiceException;
+import com.salesmanager.core.business.generic.service.SalesManagerEntityService;
+import com.salesmanager.core.business.merchant.model.MerchantStore;
+import com.salesmanager.core.business.reference.language.model.Language;
+
+public interface ContentService extends SalesManagerEntityService<Long, Content> {
+	
+	
+	public List<Content> listByType(String contentType, MerchantStore store, Language language) throws ServiceException;
+	public List<Content> listByType(List<String> contentType, MerchantStore store, Language language) throws ServiceException;
+	Content getByCode(String code, MerchantStore store)
+			throws ServiceException;
+	void saveOrUpdate(Content content) throws ServiceException;
+
+
+
+}
---
+++ sm-core/src/main/java/com/salesmanager/core/business/content/service/ContentServiceImpl.java
@@ -0,0 +1,69 @@
+package com.salesmanager.core.business.content.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.salesmanager.core.business.content.dao.ContentDao;
+import com.salesmanager.core.business.content.model.content.Content;
+import com.salesmanager.core.business.generic.exception.ServiceException;
+import com.salesmanager.core.business.generic.service.SalesManagerEntityServiceImpl;
+import com.salesmanager.core.business.merchant.model.MerchantStore;
+import com.salesmanager.core.business.reference.language.model.Language;
+
+@Service("contentService")
+public class ContentServiceImpl extends SalesManagerEntityServiceImpl<Long, Content> implements ContentService {
+	
+	private ContentDao contentDao;
+	
+
+	
+	@Autowired
+	public ContentServiceImpl(ContentDao contentDao) {
+		super(contentDao);
+		
+		this.contentDao = contentDao;
+	}
+
+
+
+	@Override
+	public List<Content> listByType(String contentType, MerchantStore store, Language language) throws ServiceException {
+
+		return contentDao.listByType(contentType, store, language);
+	}
+
+
+
+	@Override
+	public List<Content> listByType(List<String> contentType, MerchantStore store, Language language)
+			throws ServiceException {
+
+		return contentDao.listByType(contentType, store, language);
+	}
+	
+	@Override
+	public Content getByCode(String code, MerchantStore store)
+	throws ServiceException {
+		
+		return contentDao.getByCode(code, store);
+		
+	}
+	
+	@Override
+	public void saveOrUpdate(Content content) throws ServiceException {
+		
+		
+		//save or update (persist and attach entities
+		if(content.getId()!=null && content.getId()>0) {
+			super.update(content);
+		} else {
+			super.save(content);
+		}
+		
+	}
+	
+
+
+}
