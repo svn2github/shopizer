659c86d361b75d103912e2a44453d335fc0782ce
uawasthi <uawasthi@e80ee2c7-76f3-4005-aeaf-98396743f455>
2012-11-29 18:06:54 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1002 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-shop/src/main/java/com/salesmanager/web/admin/controller/content/ContentImageController.java
+++ sm-shop/src/main/java/com/salesmanager/web/admin/controller/content/ContentImageController.java
@@ -14,6 +14,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -31,6 +32,7 @@ import com.salesmanager.core.utils.ajax.AjaxResponse;
 import com.salesmanager.web.admin.controller.ControllerConstants;
 import com.salesmanager.web.admin.entity.merchant.ContentImages;
 import com.salesmanager.web.admin.entity.web.Menu;
+import com.salesmanager.web.constants.Constants;
 
 
 @Controller
