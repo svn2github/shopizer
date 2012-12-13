28222f21ab537e89c5ff37ed3ba6bd1dcd480629
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2012-12-13 18:13:59 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1067 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-shop/src/main/java/com/salesmanager/web/admin/controller/merchant/StoreBrandingController.java
+++ sm-shop/src/main/java/com/salesmanager/web/admin/controller/merchant/StoreBrandingController.java
@@ -1,17 +1,17 @@
 package com.salesmanager.web.admin.controller.merchant;
 
 
-import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,26 +19,28 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.salesmanager.core.business.content.model.image.ImageContentType;
+import com.salesmanager.core.business.content.model.image.OutputContentImage;
 import com.salesmanager.core.business.content.service.ContentService;
 import com.salesmanager.core.business.merchant.model.MerchantStore;
 import com.salesmanager.core.business.merchant.service.MerchantStoreService;
-import com.salesmanager.core.business.reference.country.model.Country;
 import com.salesmanager.core.business.reference.country.service.CountryService;
-import com.salesmanager.core.business.reference.currency.model.Currency;
 import com.salesmanager.core.business.reference.currency.service.CurrencyService;
-import com.salesmanager.core.business.reference.language.model.Language;
 import com.salesmanager.core.business.reference.language.service.LanguageService;
-import com.salesmanager.core.business.reference.zone.model.Zone;
 import com.salesmanager.core.business.reference.zone.service.ZoneService;
 import com.salesmanager.core.modules.cms.common.CMSContentImage;
+import com.salesmanager.core.utils.ajax.AjaxResponse;
 import com.salesmanager.web.admin.entity.merchant.ContentImages;
 import com.salesmanager.web.admin.entity.web.Menu;
 import com.salesmanager.web.constants.Constants;
 
 @Controller
 public class StoreBrandingController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(StoreBrandingController.class);
+	
 	
 	@Autowired
 	MerchantStoreService merchantStoreService;
@@ -99,8 +101,39 @@ public class StoreBrandingController {
 		
 		model.addAttribute("store", store);
 
-		
 		return "admin-store-branding";
+	}
+	
+	@RequestMapping(value="/admin/store/removeImage.html", method=RequestMethod.POST, produces="application/json")
+	public @ResponseBody String removeImage(HttpServletRequest request, HttpServletResponse response, Locale locale) {
+		String iid = request.getParameter("imageId");
+
+		MerchantStore store = (MerchantStore)request.getAttribute(Constants.ADMIN_STORE);
+		
+		AjaxResponse resp = new AjaxResponse();
+
+		
+		try {
+			
+			OutputContentImage contentImage = new OutputContentImage();
+			contentImage.setImageName(store.getStoreLogo());
+			contentImage.setContentType(ImageContentType.LOGO);
+			
+			contentService.removeImage(store.getCode(), contentImage);
+			
+			store.setStoreLogo(null);
+			merchantStoreService.update(store);
+		
+		
+		} catch (Exception e) {
+			LOGGER.error("Error while deleting product", e);
+			resp.setStatus(AjaxResponse.RESPONSE_STATUS_FAIURE);
+			resp.setErrorMessage(e);
+		}
+		
+		String returnString = resp.toJSONString();
+		
+		return returnString;
 	}
 	
 	private void setMenu(Model model, HttpServletRequest request) throws Exception {
