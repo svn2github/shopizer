6c9ec747b1f0bcd94e2a4024a337d09b880639c6
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2012-12-19 18:34:56 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1085 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/business/catalog/product/dao/review/ProductReviewDaoImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/catalog/product/dao/review/ProductReviewDaoImpl.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.mysema.query.jpa.JPQLQuery;
+import com.mysema.query.jpa.impl.JPAQuery;
 import com.salesmanager.core.business.catalog.product.model.Product;
+import com.salesmanager.core.business.catalog.product.model.QProduct;
 import com.salesmanager.core.business.catalog.product.model.review.ProductReview;
+import com.salesmanager.core.business.catalog.product.model.review.QProductReview;
+import com.salesmanager.core.business.catalog.product.model.review.QProductReviewDescription;
 import com.salesmanager.core.business.customer.model.Customer;
+import com.salesmanager.core.business.customer.model.QCustomer;
 import com.salesmanager.core.business.generic.dao.SalesManagerEntityDaoImpl;
 
 @Repository("productReviewDao")
@@ -17,14 +23,68 @@ public class ProductReviewDaoImpl extends SalesManagerEntityDaoImpl<Long, Produc
 
 	@Override
 	public List<ProductReview> getByCustomer(Customer customer) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		QProductReview qEntity = QProductReview.productReview;
+		QProductReviewDescription qDescription = QProductReviewDescription.productReviewDescription;
+		QCustomer qCustomer = QCustomer.customer;
+		QProduct qProduct = QProduct.product;
+		
+		JPQLQuery query = new JPAQuery (getEntityManager());
+		
+		query.from(qEntity)
+		.join(qEntity.customer,qCustomer).fetch()
+		.join(qEntity.product,qProduct).fetch()
+		.leftJoin(qProduct.merchantStore).fetch()
+		.leftJoin(qEntity.descriptions,qDescription).fetch()
+		.where(qCustomer.id.eq(customer.getId()));
+		
+		return query.list(qEntity);
+		
+		
+		
 	}
 
 	@Override
 	public List<ProductReview> getByProduct(Product product) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		
+		QProductReview qEntity = QProductReview.productReview;
+		QProductReviewDescription qDescription = QProductReviewDescription.productReviewDescription;
+		QProduct qProduct = QProduct.product;
+		
+		JPQLQuery query = new JPAQuery (getEntityManager());
+		
+		query.from(qEntity)
+		.join(qEntity.customer).fetch()
+		.join(qEntity.product,qProduct).fetch()
+		.leftJoin(qProduct.merchantStore).fetch()
+		.leftJoin(qEntity.descriptions,qDescription).fetch()
+		.where(qProduct.id.eq(product.getId()));
+		
+		return query.list(qEntity);
+		
+		
+	}
+	
+	@Override
+	public ProductReview getById(Long id) {
+
+		QProductReview qEntity = QProductReview.productReview;
+		QProductReviewDescription qDescription = QProductReviewDescription.productReviewDescription;
+		QProduct qProduct = QProduct.product;
+		
+		JPQLQuery query = new JPAQuery (getEntityManager());
+		
+		query.from(qEntity)
+			.join(qEntity.customer).fetch()
+			.join(qEntity.product,qProduct).fetch()
+			.leftJoin(qProduct.merchantStore).fetch()
+			.leftJoin(qEntity.descriptions,qDescription).fetch()
+			.where(qEntity.id.eq(id));
+		
+		return query.uniqueResult(qEntity);
+		
+		
 	}
 
 
