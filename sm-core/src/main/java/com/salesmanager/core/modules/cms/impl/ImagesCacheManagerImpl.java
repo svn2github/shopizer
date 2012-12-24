b7954089f41e90a3fe645998e23c9f28b3810cea
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2012-12-24 15:35:12 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1098 e80ee2c7-76f3-4005-aeaf-98396743f455

---
+++ sm-core/src/main/java/com/salesmanager/core/modules/cms/impl/ImagesCacheManagerImpl.java
@@ -0,0 +1,42 @@
+package com.salesmanager.core.modules.cms.impl;
+
+
+
+/**
+ * Used for managing images
+ * @author casams1
+ *
+ */
+public class ImagesCacheManagerImpl extends CacheManagerImpl {
+	
+	
+	private static  ImagesCacheManagerImpl cacheManager = null;
+	private final static String NAMED_CACHE = "DataRepository";
+	
+
+	
+
+	private ImagesCacheManagerImpl() {
+		
+		super.init(NAMED_CACHE);
+		
+		
+	}
+
+	
+	public static ImagesCacheManagerImpl getInstance() {
+		
+		if(cacheManager==null) {
+			cacheManager = new ImagesCacheManagerImpl();
+
+		}
+		
+		return cacheManager;
+		
+		
+	}
+
+
+
+}
+
