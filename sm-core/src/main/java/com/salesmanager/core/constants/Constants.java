f427392fb85b84a41a968dfaf5332eb3ad0a7aed
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-02-15 18:41:55 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1269 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/constants/Constants.java
+++ sm-core/src/main/java/com/salesmanager/core/constants/Constants.java
@@ -3,5 +3,6 @@ package com.salesmanager.core.constants;
 public class Constants {
 	
 	public static final String ALL_REGIONS = "*";
+	public final static String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
 
 }
