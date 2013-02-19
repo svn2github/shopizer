65bcb8e887227ce0678cc98e7bdc0b9f6cd47da0
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-02-19 02:01:21 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1283 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/test/java/com/salesmanager/test/customer/CustomerSalesManagerTestCase.java
+++ sm-core/src/test/java/com/salesmanager/test/customer/CustomerSalesManagerTestCase.java
@@ -6,8 +6,11 @@ import junit.framework.Assert;
 
 import org.junit.Test;
 
+import com.salesmanager.core.business.customer.model.Billing;
 import com.salesmanager.core.business.customer.model.Customer;
+import com.salesmanager.core.business.customer.model.Delivery;
 import com.salesmanager.core.business.generic.exception.ServiceException;
+import com.salesmanager.core.business.merchant.model.MerchantStore;
 import com.salesmanager.core.business.reference.country.model.Country;
 import com.salesmanager.core.business.reference.zone.model.Zone;
 import com.salesmanager.test.core.AbstractSalesManagerCoreTestCase;
@@ -16,11 +19,15 @@ public class CustomerSalesManagerTestCase extends AbstractSalesManagerCoreTestCa
 	
 	@Test
 	public void createCustomer() throws ServiceException {
+		
+		
+		MerchantStore store = merchantService.getByCode(MerchantStore.DEFAULT_STORE);
 		Country country = countryService.getByCode("CA");
 		Zone zone = zoneService.getByCode("VT");
 		
 		Customer customer = new Customer();
 		customer.setFirstname("Leonardo");
+		customer.setMerchantStore(store);
 		customer.setLastname("Ribeiro");
 		customer.setCity("city");
 		customer.setEmailAddress("test@test.com");
@@ -40,6 +47,19 @@ public class CustomerSalesManagerTestCase extends AbstractSalesManagerCoreTestCa
 		customer.setCountry(country);
 		customer.setZone(zone);
 		
+	    Delivery delivery = new Delivery();
+	    delivery.setAddress("Shipping address");
+	    delivery.setCountry(country);
+	    delivery.setZone(zone);
+	    
+	    
+	    Billing billing = new Billing();
+	    billing.setAddress("Billing address");
+	    billing.setCountry(country);
+	    billing.setZone(zone);
+	    
+	    customer.setBilling(billing);
+	    customer.setDelivery(delivery);
 		
 		customerService.create(customer);
 		customer = customerService.getById(customer.getId());
