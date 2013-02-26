318bc827b0c3e0b126fd684142831ff3d1dca3e1
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-02-26 14:43:54 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1326 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-shop/src/main/java/com/salesmanager/web/init/data/InitStoreData.java
+++ sm-shop/src/main/java/com/salesmanager/web/init/data/InitStoreData.java
@@ -1,8 +1,8 @@
 package com.salesmanager.web.init.data;
 
 import java.math.BigDecimal;
-import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,18 +28,24 @@ import com.salesmanager.core.business.catalog.product.service.image.ProductImage
 import com.salesmanager.core.business.catalog.product.service.manufacturer.ManufacturerService;
 import com.salesmanager.core.business.catalog.product.service.price.ProductPriceService;
 import com.salesmanager.core.business.catalog.product.service.type.ProductTypeService;
+import com.salesmanager.core.business.customer.model.Billing;
 import com.salesmanager.core.business.customer.model.Customer;
+import com.salesmanager.core.business.customer.model.Delivery;
 import com.salesmanager.core.business.customer.service.CustomerService;
 import com.salesmanager.core.business.generic.exception.ServiceException;
 import com.salesmanager.core.business.merchant.model.MerchantStore;
 import com.salesmanager.core.business.merchant.service.MerchantStoreService;
-import com.salesmanager.core.business.order.model.Billing;
-import com.salesmanager.core.business.order.model.Delivery;
 import com.salesmanager.core.business.order.model.Order;
+import com.salesmanager.core.business.order.model.OrderTotal;
 import com.salesmanager.core.business.order.model.orderproduct.OrderProduct;
+import com.salesmanager.core.business.order.model.orderproduct.OrderProductDownload;
+import com.salesmanager.core.business.order.model.orderproduct.OrderProductPrice;
+import com.salesmanager.core.business.order.model.orderstatus.OrderStatus;
+import com.salesmanager.core.business.order.model.orderstatus.OrderStatusHistory;
 import com.salesmanager.core.business.order.service.OrderService;
 import com.salesmanager.core.business.reference.country.model.Country;
 import com.salesmanager.core.business.reference.country.service.CountryService;
+import com.salesmanager.core.business.reference.currency.model.Currency;
 import com.salesmanager.core.business.reference.currency.service.CurrencyService;
 import com.salesmanager.core.business.reference.language.model.Language;
 import com.salesmanager.core.business.reference.language.service.LanguageService;
@@ -547,60 +553,152 @@ public class InitStoreData implements InitData {
 		    dprice6.getDescriptions().add(dpd);
 
 		    productPriceService.create(dprice6);
+
 		    
-/*		    //create a customer
+		    //Create a customer
+
 		    Customer customer = new Customer();
-			customer.setFirstname("John");
-			customer.setLastname("Doe");
+			customer.setFirstname("Leonardo");
+			customer.setMerchantStore(store);
+			customer.setLastname("DiCaprio");
 			customer.setCity("city");
 			customer.setEmailAddress("test@test.com");
-			customer.setGender("M");
+			customer.setGender("M");						
 			customer.setTelephone("444-555-6666");
 			customer.setAnonymous(true);
 			customer.setCompany("ifactory");
-			customer.setDateOfBirth(new java.util.Date());
+			customer.setDateOfBirth(new Date());
 			customer.setFax("555-666-7777");
-			customer.setPassword("123456");
-			customer.setPostalCode("123456");
-			customer.setState("state");
-			customer.setStreetAddress("Street 1");
+			customer.setNewsletter('c');
+			customer.setNick("My nick");
+			customer.setPostalCode("J4B-8J9");			
+			customer.setStreetAddress("358 Du Languadoc");
+			customer.setTelephone("444-555-6666");
 			customer.setCountry(canada);
 			customer.setZone(zone);
 			
+		    Delivery delivery = new Delivery();
+		    delivery.setAddress("358 Du Languadoc");
+		    delivery.setCity( "Boucherville" );
+		    delivery.setCountry(canada);
+		    delivery.setCountryCode(canada.getIsoCode());
+		    delivery.setName("Delivery Name" );
+		    delivery.setPostalCode("J4B-8J9" );
+		    delivery.setZone(zone);	    
+		    
+		    Billing billing = new Billing();
+		    billing.setAddress("358 Du Languadoc");
+		    billing.setCity("Boucherville");
+		    billing.setCompany("CSTI Consulting");
+		    billing.setCountry(canada);
+		    billing.setCountryCode(canada.getIsoCode());
+		    billing.setName("CSTI Consulting");
+		    billing.setPostalCode("J4B-8J9");
+		    billing.setZone(zone);
+		    
+		    customer.setBilling(billing);
+		    customer.setDelivery(delivery);		
 			customerService.create(customer);
 			
+			Currency currency = currencyService.getByCode("CAD");
+
+			OrderStatusHistory orderStatusHistory = new OrderStatusHistory();
+			
 			//create an order
-			Billing billing = new Billing();
-			billing.setName(customer.getFirstname() + " " + customer.getLastname());
-			billing.setAddress(customer.getStreetAddress());
-			billing.setCity(customer.getCity());
-			billing.setCompany(customer.getCompany());
-			billing.setCountryCode(canada.getIsoCode());
-			billing.setPostalCode(customer.getPostalCode());
-			billing.setState(zone.getName());
-			
-			Delivery delivery = new Delivery();
-			delivery.setName(customer.getFirstname() + " " + customer.getLastname());
-			delivery.setAddress(customer.getStreetAddress());
-			delivery.setCity(customer.getCity());
-			delivery.setCompany(customer.getCompany());
-			delivery.setCountryCode(canada.getIsoCode());
-			delivery.setPostalCode(customer.getPostalCode());
-			delivery.setState(zone.getName());
-			
-			//create an OrderProduct
-			
-			//OrderProductPrice
-			//TODO remove Product from PrderProduct
-			
-			OrderProduct orderProduct = new OrderProduct();
-			//orderProduct.s
 			
 			Order order = new Order();
+			order.setDatePurchased(new Date());
+			order.setCurrency(currency);
+			order.setLastModified(new Date());
 			order.setBilling(billing);
+
+
+			order.setChannel(1);//1 is online
+			order.setCurrencyValue(new BigDecimal(0.98));//compared to based currency (not necessary)
+			order.setCustomerId(customer.getId());
 			order.setDelivery(delivery);
-			//order.
-*/		
+			order.setDisplayInvoicePayments(true);
+			order.setIpAddress("ipAddress" );
+			order.setMerchant(store);
+			order.setOrderDateFinished(new Date());//committed date
+			
+			orderStatusHistory.setComments("We received your order");
+			orderStatusHistory.setCustomerNotified(1);
+			orderStatusHistory.setStatus(OrderStatus.ORDERED);
+			orderStatusHistory.setDateAdded(new Date() );
+			orderStatusHistory.setOrder(order);
+			order.getOrderHistory().add( orderStatusHistory );		
+			
+			order.setOrderTax(new BigDecimal(4.00));
+			order.setPaymentMethod("Paypal");
+			order.setPaymentModuleCode("paypal");
+			order.setStatus( OrderStatus.DELIVERED);
+			order.setTotal(new BigDecimal(23.99));
+			
+			
+			//OrderProductDownload - Digital download
+			OrderProductDownload orderProductDownload = new OrderProductDownload();
+			orderProductDownload.setDownloadCount(1);
+			orderProductDownload.setFileId( new Long(1) );
+			orderProductDownload.setMaxdays(31);		
+			orderProductDownload.setOrderProductFilename("Your digital file name");
+			
+			//OrderProductPrice
+			OrderProductPrice oproductprice = new OrderProductPrice();
+			oproductprice.setDefaultPrice(true);	
+			oproductprice.setProductPriceAmount(new BigDecimal(19.99) );
+			oproductprice.setProductPriceCode("baseprice" );
+			oproductprice.setProductPriceName("Base Price" );
+			//oproductprice.setProductPriceSpecialAmount(new BigDecimal(13.99) );	
+
+			
+			//OrderProduct
+			OrderProduct oproduct = new OrderProduct();
+			oproduct.getDownloads().add( orderProductDownload);
+			oproduct.setFinalPrice(new BigDecimal(19.99) );
+			oproduct.setOnetimeCharge( new BigDecimal(19.99) );
+			oproduct.setOrder(order);		
+			oproduct.setProductName( "Product name" );
+			oproduct.setProductQuantity(1);
+			oproduct.setSku("TB12345" );		
+			oproduct.getPrices().add(oproductprice ) ;
+			
+			oproductprice.setOrderProduct(oproduct);		
+			orderProductDownload.setOrderProduct(oproduct);
+			order.getOrderProducts().add(oproduct);
+
+			//OrderTotal
+			OrderTotal subtotal = new OrderTotal();	
+			subtotal.setModule("summary" );		
+			subtotal.setSortOrder(0);
+			subtotal.setText("Summary" );
+			subtotal.setTitle("Summary" );
+			subtotal.setValue(new BigDecimal(19.99 ) );
+			subtotal.setOrder(order);
+			
+			order.getOrderTotal().add(subtotal);
+			
+			OrderTotal tax = new OrderTotal();	
+			tax.setModule("tax" );		
+			tax.setSortOrder(1);
+			tax.setText("Tax" );
+			tax.setTitle("Tax" );
+			tax.setValue(new BigDecimal(4) );
+			tax.setOrder(order);
+			
+			order.getOrderTotal().add(tax);
+			
+			OrderTotal total = new OrderTotal();	
+			total.setModule("total" );		
+			total.setSortOrder(2);
+			total.setText("Total" );
+			total.setTitle("Total" );
+			total.setValue(new BigDecimal(23.99) );
+			total.setOrder(order);
+			
+			order.getOrderTotal().add(total);
+			
+			orderService.create(order);		
 		
 	}
 
