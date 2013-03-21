4108484c637809433dac5e7744531240e3a0befa
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-03-21 13:38:49 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1395 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/business/payments/model/Transaction.java
+++ sm-core/src/main/java/com/salesmanager/core/business/payments/model/Transaction.java
@@ -12,9 +12,11 @@ import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.TableGenerator;
@@ -49,8 +51,9 @@ public class Transaction extends SalesManagerEntity<Long, Transaction> implement
 	@Embedded
 	private AuditSection auditSection = new AuditSection();
 
-	@ManyToOne(targetEntity = Order.class)
-	@Column(name="ORDER_ID")
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="ORDER_ID", nullable=false)
 	private Order order;
 	
 	@Column(name="AMOUNT")
