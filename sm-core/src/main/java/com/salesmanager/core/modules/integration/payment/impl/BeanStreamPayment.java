a122754f56709fba40f5df4c8823f65c6bd63ec1
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-04-03 17:29:18 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1455 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/modules/integration/payment/impl/BeanStreamPayment.java
+++ sm-core/src/main/java/com/salesmanager/core/modules/integration/payment/impl/BeanStreamPayment.java
@@ -31,6 +31,7 @@ import com.salesmanager.core.business.system.model.MerchantLog;
 import com.salesmanager.core.business.system.service.MerchantLogService;
 import com.salesmanager.core.modules.integration.IntegrationException;
 import com.salesmanager.core.modules.integration.payment.model.PaymentModule;
+import com.salesmanager.core.utils.CreditCardUtils;
 import com.salesmanager.core.utils.ProductPriceUtils;
 
 public class BeanStreamPayment implements PaymentModule {
@@ -293,6 +294,189 @@ public class BeanStreamPayment implements PaymentModule {
 
 		
 	}
+	
+	
+	
+	private Transaction processTransaction(TransactionType type,
+			Order order,
+			BigDecimal amount, Payment payment,
+			IntegrationConfiguration configuration, IntegrationModule module) throws IntegrationException {
+		
+
+		
+		
+		
+		boolean bSandbox = false;
+		if (configuration.getEnvironment().equals("TEST")) {// sandbox
+			bSandbox = true;
+		}
+
+		String server = "";
+
+
+		if (bSandbox == true) {
+			server = new StringBuffer().append(
+					module.getModuleConfigs().get("scheme")).append("://")
+					.append(module.getModuleConfigs().get("host")).append(
+							module.getModuleConfigs().get("uri")).toString();
+		} else {
+
+			server = new StringBuffer().append(
+					module.getModuleConfigs().get("scheme")).append("://")
+					.append(module.getModuleConfigs().get("host")).append(
+							module.getModuleConfigs().get("uri")).toString();
+		}
+		
+		HttpURLConnection conn = null;
+		
+		try {
+			
+		
+		String orderNumber = new StringBuilder().append(order.getId()).append(new Date().getTime()).toString();
+		
+		String amnt = productPriceUtils.getAdminFormatedAmount(order.getMerchant(), order.getTotal());
+		
+		
+		StringBuilder messageString = new StringBuilder();
+		
+
+		messageString.append("requestType=BACKEND&");
+		messageString.append("merchant_id=").append(configuration.getIntegrationKeys().get("MERCHANT_ID")).append("&");
+		messageString.append("trnType=").append(type).append("&");
+		messageString.append("orderNumber=").append(orderNumber).append("&");
+		messageString.append("trnCardOwner=").append(order.getCcOwner()).append("&");
+		messageString.append("trnCardNumber=").append(order.getCcNumber()).append("&");
+		messageString.append("trnExpMonth=").append(order.getCcExpires().substring(0, 2)).append("&");
+		messageString.append("trnExpYear=").append(order.getCcExpires().substring(2,
+				order.getCcExpires().length())).append("&");
+		messageString.append("trnCardCvd=").append(order.getCcCvv()).append("&");
+		messageString.append("trnAmount=").append(amnt).append("&");
+		messageString.append("ordName=").append(order.getBilling().getName()).append("&");
+		messageString.append("ordAddress1=").append(order.getBilling().getAddress()).append("&");
+		messageString.append("ordCity=").append(order.getBilling().getCity()).append("&");
+		
+
+		
+		messageString.append("ordProvince=").append(order.getBilling().getState()).append("&");
+		messageString.append("ordPostalCode=").append(order.getBilling().getPostalCode()).append("&");
+		messageString.append("ordCountry=").append(order.getCustomerCountry()).append("&");
+		messageString.append("ordPhoneNumber=").append(order.getCustomerTelephone()).append("&");
+		messageString.append("ordEmailAddress=").append(order.getCustomerEmailAddress());
+		
+		
+		
+		
+		/**
+		 * 	purchase (P)
+		 *  -----------
+				REQUEST -> merchant_id=123456789&requestType=BACKEND&trnType=P&trnOrderNumber=1234TEST&trnAmount=5.00&trnCardOwner=Joe+Test&trnCardNumber=4030000010001234&trnExpMonth=10&trnExpYear=10&ordName=Joe+Test&ordAddress1=123+Test+Street&ordCity=Victoria&ordProvince=BC&ordCountry=CA&ordPostalCode=V8T2E7&ordPhoneNumber=5555555555&ordEmailAddress=joe%40testemail.com
+				RESPONSE-> trnApproved=1&trnId=10003067&messageId=1&messageText=Approved&trnOrderNumber=E40089&authCode=TEST&errorType=N&errorFields=&responseType=T&trnAmount=10%2E00&trnDate=1%2F17%2F2008+11%3A36%3A34+AM&avsProcessed=0&avsId=0&avsResult=0&avsAddrMatch=0&avsPostalMatch=0&avsMessage=Address+Verification+not+performed+for+this+transaction%2E&rspCodeCav=0&rspCavResult=0&rspCodeCredit1=0&rspCodeCredit2=0&rspCodeCredit3=0&rspCodeCredit4=0&rspCodeAddr1=0&rspCodeAddr2=0&rspCodeAddr3=0&rspCodeAddr4=0&rspCodeDob=0&rspCustomerDec=&trnType=P&paymentMethod=CC&ref1=&ref2=&ref3=&ref4=&ref5=
+		
+			pre authorization (PA)
+			----------------------
+
+			Prior to processing a pre-authorization through the API, you must modify the transaction settings in your Beanstream merchant member area to allow for this transaction type.
+			- Log in to the Beanstream online member area at www.beanstream.com/admin/sDefault.asp.
+			- Navigate to administration - account admin - order settings in the left menu.
+			Under the heading �Restrict Internet Transaction Processing Types,� select either of the last two options. The �Purchases or Pre-Authorization Only� option will allow you to process both types of transaction through your web interface. De-selecting the �Restrict Internet Transaction Processing Types� checkbox will allow you to process all types of transactions including returns, voids and pre-auth completions.
+		
+			capture (PAC) -> requires trnId
+			-------------
+		
+			refund (R)
+			-------------
+				REQUEST -> merchant_id=123456789&requestType=BACKEND&trnType=R&username=user1234&password=pass1234&trnOrderNumber=1234&trnAmount=1.00&adjId=10002115
+				RESPONSE-> trnApproved=1&trnId=10002118&messageId=1&messageText=Approved&trnOrderNumber=1234R&authCode=TEST&errorType=N&errorFields=&responseType=T&trnAmount=1%2E00&trnDate=8%2F17%2F2009+1%3A44%3A56+PM&avsProcessed=0&avsId=0&avsResult=0&avsAddrMatch=0&avsPostalMatch=0&avsMessage=Address+Verification+not+performed+for+this+transaction%2E&cardType=VI&trnType=R&paymentMethod=CC&ref1=&ref2=&ref3=&ref4=&ref5=
+		
+
+			//notes
+			//On receipt of the transaction response, the merchant must display order amount, transaction ID number, bank authorization code (authCode), currency, date and �messageText� to the customer on a confirmation page.
+		*/
+		
+
+		//String agent = "Mozilla/4.0";
+		//String respText = "";
+		//Map nvp = null;
+		
+		
+		/** debug **/
+		
+		
+
+			StringBuffer messageLogString = new StringBuffer();
+			
+			
+			messageLogString.append("requestType=BACKEND&");
+			messageLogString.append("merchant_id=").append(configuration.getIntegrationKeys().get("MERCHANT_ID")).append("&");
+			messageLogString.append("trnType=").append(type).append("&");
+			messageLogString.append("orderNumber=").append(orderNumber).append("&");
+			messageLogString.append("trnCardOwner=").append(order.getCcOwner()).append("&");
+			messageLogString.append("trnCardNumber=").append(CreditCardUtils.maskCardNumber(order.getCcNumber())).append("&");
+			messageLogString.append("trnExpMonth=").append(order.getCcExpires().substring(0, 2)).append("&");
+			messageString.append("trnExpYear=").append(order.getCcExpires().substring(2,
+					order.getCcExpires().length())).append("&");
+			messageLogString.append("trnCardCvd=").append(order.getCcCvv()).append("&");
+			messageLogString.append("trnAmount=").append(amnt).append("&");
+			messageLogString.append("ordName=").append(order.getBilling().getName()).append("&");
+			messageLogString.append("ordAddress1=").append(order.getBilling().getAddress()).append("&");
+			messageLogString.append("ordCity=").append(order.getBilling().getCity()).append("&");
+			
+
+			
+			messageLogString.append("ordProvince=").append(order.getBilling().getState()).append("&");
+			messageLogString.append("ordPostalCode=").append(order.getBilling().getPostalCode()).append("&");
+			messageLogString.append("ordCountry=").append(order.getCustomerCountry()).append("&");
+			messageLogString.append("ordPhoneNumber=").append(order.getCustomerTelephone()).append("&");
+			messageLogString.append("ordEmailAddress=").append(order.getCustomerEmailAddress());
+			
+		
+
+			/** debug **/
+	
+	
+			LOGGER.debug("REQUEST SENT TO BEANSTREAM -> " + messageLogString.toString());
+
+		
+
+		
+		
+		//DataOutputStream output = null;
+		//DataInputStream in = null;
+		//BufferedReader is = null;
+	
+			
+			URL postURL = new URL(server.toString());
+			conn = (HttpURLConnection) postURL.openConnection();
+			
+
+
+			
+			Transaction response = this.sendTransaction(messageString.toString(), type.name(), order, conn);
+			
+			return response;
+
+
+			
+		} catch(Exception e) {
+			
+			if(e instanceof IntegrationException)
+				throw (IntegrationException)e;
+			throw new IntegrationException("Error while processing BeanStream transaction",e);
+
+		} finally {
+			
+			
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ignore) {
+					// TODO: handle exception
+				}
+			}
+		}
+
+	}
+	
 	
 	
 	private Transaction parseResponse(TransactionType transactionType,
