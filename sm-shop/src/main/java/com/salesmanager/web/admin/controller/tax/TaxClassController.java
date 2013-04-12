e283e352e29ea7d56523cf329dd8385a655a0366
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-04-12 17:40:38 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1491 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-shop/src/main/java/com/salesmanager/web/admin/controller/tax/TaxClassController.java
+++ sm-shop/src/main/java/com/salesmanager/web/admin/controller/tax/TaxClassController.java
@@ -1,21 +1,119 @@
 package com.salesmanager.web.admin.controller.tax;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.salesmanager.core.business.merchant.model.MerchantStore;
+import com.salesmanager.core.business.tax.model.taxclass.TaxClass;
+import com.salesmanager.core.business.tax.service.TaxClassService;
+import com.salesmanager.core.utils.ajax.AjaxPageableResponse;
+import com.salesmanager.core.utils.ajax.AjaxResponse;
+import com.salesmanager.web.admin.entity.web.Menu;
+import com.salesmanager.web.constants.Constants;
 
 @Controller
 public class TaxClassController {
 	
+	@Autowired
+	private TaxClassService taxClassService = null;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(TaxClassController.class);
+	private final static String DEFAULT_TAX_CLASS  = "DEFAULT";
+	
+	@Secured("TAX")
 	@RequestMapping(value={"/admin/tax/taxclass/list.html"}, method=RequestMethod.GET)
 	public String displayTaxClasses(Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
+		setMenu(model, request);
+		return com.salesmanager.web.admin.controller.ControllerConstants.Tiles.Tax.taxClasses;
+	}
+	
+	
+	@Secured("TAX")
+	@RequestMapping(value = "/admin/tax/taxclass/paging.html", method = RequestMethod.POST, produces = "application/json")
+	public @ResponseBody
+	String pageTaxClasses(HttpServletRequest request,
+			HttpServletResponse response, Locale locale) {
+		
+		MerchantStore store = (MerchantStore)request.getAttribute(Constants.ADMIN_STORE);
+
+		AjaxResponse resp = new AjaxResponse();
+		try {
+
+				List<TaxClass> taxClasses = taxClassService.listByStore(store);
+				for(TaxClass tax : taxClasses) {
+					
+					Map<String,String> entry = new HashMap<String,String>();
+					entry.put("taxClassId", String.valueOf(tax.getId()));
+					entry.put("code", tax.getCode());
+					entry.put("name", tax.getTitle());
+					resp.addDataEntry(entry);
+				}
+
+				resp.setStatus(AjaxPageableResponse.RESPONSE_STATUS_SUCCESS);
+		
+		} catch (Exception e) {
+			LOGGER.error("Error while paging permissions", e);
+			resp.setStatus(AjaxPageableResponse.RESPONSE_STATUS_FAIURE);
+			resp.setErrorMessage(e);
+		}
+		
+		String returnString = resp.toJSONString();
+		return returnString;
+	}
+	
+	@Secured("TAX")
+	@RequestMapping(value="/admin/tax/taxclass/save.html", method=RequestMethod.POST)
+	public String saveUser(@Valid @ModelAttribute("taxClass") TaxClass taxClass, BindingResult result, Model model, HttpServletRequest request, Locale locale) throws Exception {
+		
+		
+		setMenu(model, request);
+		
+		taxClassService.create(taxClass);
+		
+		model.addAttribute("success","success");
 		
 		return com.salesmanager.web.admin.controller.ControllerConstants.Tiles.Tax.taxClasses;
+		
+	}
+
+
+	
+	private void setMenu(Model model, HttpServletRequest request)
+	throws Exception {
+
+		// display menu
+		Map<String, String> activeMenus = new HashMap<String, String>();
+		activeMenus.put("tax", "tax");
+		activeMenus.put("taxclass", "taxclass");
+		
+		@SuppressWarnings("unchecked")
+		Map<String, Menu> menus = (Map<String, Menu>) request
+				.getAttribute("MENUMAP");
+		
+		Menu currentMenu = (Menu) menus.get("tax");
+		model.addAttribute("currentMenu", currentMenu);
+		model.addAttribute("activeMenus", activeMenus);
+		//
+
 	}
 
 }
