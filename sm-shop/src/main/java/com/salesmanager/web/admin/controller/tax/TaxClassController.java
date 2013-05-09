c43fd3675889db798834c36f83040194db579b76
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-05-09 17:54:04 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1611 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-shop/src/main/java/com/salesmanager/web/admin/controller/tax/TaxClassController.java
+++ sm-shop/src/main/java/com/salesmanager/web/admin/controller/tax/TaxClassController.java
@@ -24,10 +24,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.salesmanager.core.business.merchant.model.MerchantStore;
 import com.salesmanager.core.business.tax.model.taxclass.TaxClass;
 import com.salesmanager.core.business.tax.service.TaxClassService;
+import com.salesmanager.core.business.user.model.User;
 import com.salesmanager.core.utils.ajax.AjaxPageableResponse;
 import com.salesmanager.core.utils.ajax.AjaxResponse;
 import com.salesmanager.web.admin.entity.web.Menu;
 import com.salesmanager.web.constants.Constants;
+import com.salesmanager.web.utils.LabelUtils;
+import com.salesmanager.web.utils.UserUtils;
 
 @Controller
 public class TaxClassController {
@@ -35,8 +38,11 @@ public class TaxClassController {
 	@Autowired
 	private TaxClassService taxClassService = null;
 	
+	@Autowired
+	LabelUtils messages;
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(TaxClassController.class);
-	private final static String DEFAULT_TAX_CLASS  = "DEFAULT";
+
 	
 	@Secured("TAX")
 	@RequestMapping(value={"/admin/tax/taxclass/list.html"}, method=RequestMethod.GET)
@@ -66,12 +72,13 @@ public class TaxClassController {
 
 				List<TaxClass> taxClasses = taxClassService.listByStore(store);
 				for(TaxClass tax : taxClasses) {
-					
-					Map<String,String> entry = new HashMap<String,String>();
-					entry.put("taxClassId", String.valueOf(tax.getId()));
-					entry.put("code", tax.getCode());
-					entry.put("name", tax.getTitle());
-					resp.addDataEntry(entry);
+					if(!tax.getCode().equals(TaxClass.DEFAULT_TAX_CLASS)) {
+						Map<String,String> entry = new HashMap<String,String>();
+						entry.put("taxClassId", String.valueOf(tax.getId()));
+						entry.put("code", tax.getCode());
+						entry.put("name", tax.getTitle());
+						resp.addDataEntry(entry);
+					}
 				}
 
 				resp.setStatus(AjaxPageableResponse.RESPONSE_STATUS_SUCCESS);
@@ -98,6 +105,64 @@ public class TaxClassController {
 		model.addAttribute("success","success");
 		
 		return com.salesmanager.web.admin.controller.ControllerConstants.Tiles.Tax.taxClasses;
+		
+	}
+	
+	
+	@Secured("TAX")
+	@RequestMapping(value="/admin/tax/taxclass/remove.html", method=RequestMethod.POST, produces="application/json")
+	public @ResponseBody String removeTaxClass(HttpServletRequest request, Locale locale) throws Exception {
+		
+		//do not remove super admin
+		
+		String taxClassId = request.getParameter("taxClassId");
+
+		AjaxResponse resp = new AjaxResponse();
+		
+
+		try {
+			
+
+			/**
+			 * In order to remove a User the logged in ser must be STORE_ADMIN
+			 * or SUPER_USER
+			 */
+			
+
+			if(taxClassId==null){
+				resp.setStatusMessage(messages.getMessage("message.unauthorized", locale));
+				resp.setStatus(AjaxResponse.RESPONSE_STATUS_FAIURE);			
+				return resp.toJSONString();
+			}
+			
+			long lTaxClassId;
+			try {
+				lTaxClassId = Long.parseLong(taxClassId);
+			} catch (Exception e) {
+				LOGGER.error("Invalid taxClassId " + taxClassId);
+				resp.setStatusMessage(messages.getMessage("message.unauthorized", locale));
+				resp.setStatus(AjaxResponse.RESPONSE_STATUS_FAIURE);			
+				return resp.toJSONString();
+			}
+
+			
+			TaxClass taxClass = taxClassService.getById(lTaxClassId);
+			
+			taxClassService.delete(taxClass);
+			
+			resp.setStatus(AjaxResponse.RESPONSE_OPERATION_COMPLETED);
+
+		
+		
+		} catch (Exception e) {
+			LOGGER.error("Error while deleting product price", e);
+			resp.setStatus(AjaxResponse.RESPONSE_STATUS_FAIURE);
+			resp.setErrorMessage(e);
+		}
+		
+		String returnString = resp.toJSONString();
+		
+		return returnString;
 		
 	}
 
