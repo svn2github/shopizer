4e4577232466660594b497982b7d25933881b092
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-05-23 17:28:01 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1655 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/test/java/com/salesmanager/test/content/StaticContentTestCase.java
+++ sm-core/src/test/java/com/salesmanager/test/content/StaticContentTestCase.java
@@ -1,17 +1,15 @@
 package com.salesmanager.test.content;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
-import java.io.ByteArrayOutputStream;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,17 +18,13 @@ import com.salesmanager.core.business.content.model.content.StaticContentType;
 import com.salesmanager.core.business.content.service.StaticContentService;
 import com.salesmanager.core.business.generic.exception.ServiceException;
 import com.salesmanager.core.business.merchant.model.MerchantStore;
-import com.salesmanager.core.business.reference.country.model.Country;
-import com.salesmanager.core.business.reference.language.model.Language;
-import com.salesmanager.core.business.reference.zone.model.Zone;
 import com.salesmanager.core.modules.cms.common.InputStaticContentData;
 import com.salesmanager.core.modules.cms.common.OutputStaticContentData;
 import com.salesmanager.test.core.AbstractSalesManagerCoreTestCase;
 
 public class StaticContentTestCase extends AbstractSalesManagerCoreTestCase {
 	
-	private static final Date date = new Date(System.currentTimeMillis());
-	
+
 	@Autowired
 	private StaticContentService staticContentService;
 	
@@ -57,11 +51,13 @@ public class StaticContentTestCase extends AbstractSalesManagerCoreTestCase {
 	     staticContent.setFileName(file.getName());
 	     staticContent.setContentType(StaticContentType.STATIC_DATA);//default to static data
         
-	     staticContentService.addStaticContentData(store.getCode(), staticContent);
+	     staticContentService.addStaticContentData(store, staticContent);
+	     
+	     staticContentService.getStaticContentData(store, StaticContentType.STATIC_DATA, file.getName());
 
         //now get the file
 	     
-	     OutputStaticContentData getData = staticContentService.getStaticContentData(store.getCode(), StaticContentType.STATIC_DATA ,file.getName());
+/*	     OutputStaticContentData getData = staticContentService.getStaticContentData(store, StaticContentType.STATIC_DATA ,file.getName());
 	     Assert.assertNotNull(getData);
 	     if(getData != null) {
 	    	 
@@ -73,12 +69,69 @@ public class StaticContentTestCase extends AbstractSalesManagerCoreTestCase {
 	    	 ByteArrayOutputStream baos =  getData.getFile();
 	    	 baos.writeTo(outputStream);
 	    	 
-	     }
+	     }*/
+	     
+	     //remove the file
+	     
+	     staticContentService.removeFile(store, StaticContentType.STATIC_DATA, file.getName());
 
 	}
 	
 	@Test
 	public void testCreateMultipleStaticContent() throws Exception {
+		
+		
+	    MerchantStore store = merchantService.getByCode(MerchantStore.DEFAULT_STORE);
+	    
+	    
+	    // FILE 1
+        final File file = new File( "c:/doc/carl/cdbaby.zip" );
+
+        if ( !file.exists() || !file.canRead() )
+        {
+            throw new ServiceException( "Can't read" + file.getAbsolutePath() );
+        }
+
+        byte[] is;
+        ByteArrayInputStream inputStream = null;
+
+		is = IOUtils.toByteArray( new FileInputStream( file ) );
+		inputStream = new ByteArrayInputStream( is );
+
+	    InputStaticContentData staticContent = new InputStaticContentData();
+	    staticContent.setFile(inputStream);
+	    staticContent.setFileName(file.getName());
+	    staticContent.setContentType(StaticContentType.STATIC_DATA);//default to static data
+	    
+	    
+	    // FILE 2
+        final File file2 = new File( "c:/doc/carl/Cocoa - Requirements.doc" );
+
+        if ( !file2.exists() || !file2.canRead() )
+        {
+            throw new ServiceException( "Can't read" + file2.getAbsolutePath() );
+        }
+
+        byte[] is2;
+        ByteArrayInputStream inputStream2 = null;
+
+		is2 = IOUtils.toByteArray( new FileInputStream( file2 ) );
+		inputStream2 = new ByteArrayInputStream( is2 );
+
+	    InputStaticContentData staticContent2 = new InputStaticContentData();
+	    staticContent2.setFile(inputStream2);
+	    staticContent2.setFileName(file2.getName());
+	    staticContent2.setContentType(StaticContentType.STATIC_DATA);//default to static data
+	    
+	    List<InputStaticContentData> staticFiles = new ArrayList<InputStaticContentData>();
+	    staticFiles.add(staticContent);
+	    staticFiles.add(staticContent2);
+		
+	    staticContentService.addStaticContentDataFiles(store, staticFiles);
+	    
+	    //get file names
+	    //staticContentService.get
+		
 		
 	}
 	
