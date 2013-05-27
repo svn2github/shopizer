1b1cac1ab670b7dbf33e9ee79ff65e7f2c2d26ab
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-05-27 19:25:16 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1675 e80ee2c7-76f3-4005-aeaf-98396743f455

---
+++ sm-core/src/main/java/com/salesmanager/core/business/content/model/content/ImageContentFile.java
@@ -0,0 +1,23 @@
+package com.salesmanager.core.business.content.model.content;
+
+import java.awt.image.BufferedImage;
+import java.io.Serializable;
+
+public class ImageContentFile extends InputContentFile implements Serializable {
+
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5321162403524229224L;
+	private BufferedImage bufferedImage;
+
+	public void setBufferedImage(BufferedImage bufferedImage) {
+		this.bufferedImage = bufferedImage;
+	}
+
+	public BufferedImage getBufferedImage() {
+		return bufferedImage;
+	}
+}
