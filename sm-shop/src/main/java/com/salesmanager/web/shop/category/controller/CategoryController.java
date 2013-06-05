5abc107fbd430a16d50f8c4c6b9e7f8bdc8cc68e
kayragz <kayragz@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-06-05 10:36:55 +0000

    Creation of package and controller that accepts search friendly url
    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1720 e80ee2c7-76f3-4005-aeaf-98396743f455

---
+++ sm-shop/src/main/java/com/salesmanager/web/shop/category/controller/CategoryController.java
@@ -0,0 +1,17 @@
+package com.salesmanager.web.shop.category.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class CategoryController {
+	
+	public String getCategory(){
+		
+		return " ";
+	}
+
+	
+
+}
