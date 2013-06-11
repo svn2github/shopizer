39e6d9a15e2dada21e2b5694374911f5502e7e89
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-06-11 19:28:08 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1749 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/business/catalog/product/service/file/DigitalProductServiceImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/catalog/product/service/file/DigitalProductServiceImpl.java
@@ -2,21 +2,68 @@ package com.salesmanager.core.business.catalog.product.service.file;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import com.salesmanager.core.business.catalog.product.dao.file.DigitalProductDao;
+import com.salesmanager.core.business.catalog.product.model.Product;
 import com.salesmanager.core.business.catalog.product.model.file.DigitalProduct;
+import com.salesmanager.core.business.content.model.content.InputContentFile;
+import com.salesmanager.core.business.generic.exception.ServiceException;
 import com.salesmanager.core.business.generic.service.SalesManagerEntityServiceImpl;
+import com.salesmanager.core.modules.cms.content.ContentFileManager;
 
 @Service("digitalProductService")
 public class DigitalProductServiceImpl extends SalesManagerEntityServiceImpl<Long, DigitalProduct> 
 	implements DigitalProductService {
 	
 	private DigitalProductDao digitalProductDao;
+	
+    @Autowired
+    ContentFileManager contentFileManager;
 
 	@Autowired
 	public DigitalProductServiceImpl(DigitalProductDao digitalProductDao) {
 		super(digitalProductDao);
 		this.digitalProductDao = digitalProductDao;
+	}
+	
+	
+	public void addProductFile(Product product, DigitalProduct digitalProduct, InputContentFile inputFile) throws ServiceException {
+	
+		digitalProduct.setProduct(product);
+
+		try {
+			
+			Assert.notNull(inputFile.getFile(),"InputContentFile.file cannot be null");
+
+
+			
+			//contentFileManager.
+	
+			//insert ProductImage
+			//this.saveOrUpdate(productImage);
+			
+
+		
+		} catch (Exception e) {
+			throw new ServiceException(e);
+		} finally {
+			try {
+				
+/*				if(inputImage.getBufferedImage()!=null){
+					inputImage.getBufferedImage().flush();
+				}
+				
+				if(inputImage.getFile()!=null) {
+					inputImage.getFile().close();
+				}*/
+
+			} catch(Exception ignore) {
+				
+			}
+		}
+		
+		
 	}
 	
 
