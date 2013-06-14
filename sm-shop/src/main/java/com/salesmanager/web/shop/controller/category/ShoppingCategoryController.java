186fb552135fd348248f4a88925950825a4877f7
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-06-14 13:33:47 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1758 e80ee2c7-76f3-4005-aeaf-98396743f455

---
+++ sm-shop/src/main/java/com/salesmanager/web/shop/controller/category/ShoppingCategoryController.java
@@ -0,0 +1,27 @@
+package com.salesmanager.web.shop.controller.category;
+
+
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class ShoppingCategoryController {
+	
+	public String getCategory(){
+		
+		return " ";
+	}
+	
+	//carl -> a suggestion of the method signature
+	
+/*	@RequestMapping("/shop/category/{friendlyUrl}.html")
+	public String displayCategory(@PathVariable final String friendlyUrl, Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		
+		
+		return "";
+	}*/
+
+	
+
+}
