350c0f8a64216825312334fa7062d296bdf3083d
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-06-17 13:28:54 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1763 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/test/java/com/salesmanager/test/order/OrderSalesManagerTestCase.java
+++ sm-core/src/test/java/com/salesmanager/test/order/OrderSalesManagerTestCase.java
@@ -175,7 +175,6 @@ public class OrderSalesManagerTestCase extends AbstractSalesManagerCoreTestCase 
 		//OrderProductDownload - Digital download
 		OrderProductDownload orderProductDownload = new OrderProductDownload();
 		orderProductDownload.setDownloadCount(1);
-		orderProductDownload.setFileId( new Long(1) );
 		orderProductDownload.setMaxdays(31);		
 		orderProductDownload.setOrderProductFilename("Your digital file name");
 		
