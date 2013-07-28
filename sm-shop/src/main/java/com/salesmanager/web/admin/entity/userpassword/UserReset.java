59bc1085a9964f66f53b9d7df32bf580be0f648e
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-07-28 23:37:20 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@1925 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-shop/src/main/java/com/salesmanager/web/admin/entity/userpassword/UserReset.java
+++ sm-shop/src/main/java/com/salesmanager/web/admin/entity/userpassword/UserReset.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class UserReset
 {
-  final String CHAR_LIST = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  final static String CHAR_LIST = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
   final int RANDOM_STRING_LENGTH = 10;
 
@@ -14,7 +14,7 @@ public class UserReset
     StringBuffer randStr = new StringBuffer();
     for (int i = 0; i < 10; i++) {
       int number = getRandomNumber();
-      char ch = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".charAt(number);
+      char ch = CHAR_LIST.charAt(number);
       randStr.append(ch);
     }
     return randStr.toString();
@@ -24,7 +24,7 @@ public class UserReset
   {
     int randomInt = 0;
     Random randomGenerator = new Random();
-    randomInt = randomGenerator.nextInt("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".length());
+    randomInt = randomGenerator.nextInt(CHAR_LIST.length());
     if (randomInt - 1 == -1) {
       return randomInt;
     }
