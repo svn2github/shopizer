10ab767c993d70abee6c5fd140c38f4f9e892c05
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-09-05 03:28:26 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@2116 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/business/customer/dao/attribute/CustomerOptionSetDaoImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/customer/dao/attribute/CustomerOptionSetDaoImpl.java
@@ -2,6 +2,8 @@ package com.salesmanager.core.business.customer.dao.attribute;
 
 import java.util.List;
 
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
 
 import com.mysema.query.jpa.JPQLQuery;
@@ -14,6 +16,7 @@ import com.salesmanager.core.business.customer.model.attribute.QCustomerOptionVa
 import com.salesmanager.core.business.customer.model.attribute.QCustomerOptionValueDescription;
 import com.salesmanager.core.business.generic.dao.SalesManagerEntityDaoImpl;
 import com.salesmanager.core.business.merchant.model.MerchantStore;
+import com.salesmanager.core.business.merchant.model.QMerchantStore;
 import com.salesmanager.core.business.reference.language.model.Language;
 
 @Repository("customerOptionSetDao")
@@ -67,25 +70,51 @@ public class CustomerOptionSetDaoImpl extends SalesManagerEntityDaoImpl<Long, Cu
 	public List<CustomerOptionSet> listByStore(MerchantStore store, Language language) {
 		QCustomerOptionSet qCustomerOptionSet= QCustomerOptionSet.customerOptionSet;
 		QCustomerOption qCustomerOption = QCustomerOption.customerOption;
+		QMerchantStore qMerchantStore = QMerchantStore.merchantStore;
 		QCustomerOptionValue qCustomerOptionValue = QCustomerOptionValue.customerOptionValue;
 		QCustomerOptionDescription qCustomerOptionDescription = QCustomerOptionDescription.customerOptionDescription;
 		QCustomerOptionValueDescription qCustomerOptionValueDescription = QCustomerOptionValueDescription.customerOptionValueDescription;
 		
 		
-		JPQLQuery query = new JPAQuery (getEntityManager());
+		//JPQLQuery query = new JPAQuery (getEntityManager());
 		
-		query.from(qCustomerOptionSet)
-			.join(qCustomerOptionSet.pk.customerOption,qCustomerOption).fetch()
-			.join(qCustomerOptionSet.pk.customerOptionValue,qCustomerOptionValue).fetch()
-			.leftJoin(qCustomerOption.descriptions,qCustomerOptionDescription).fetch()
-			.leftJoin(qCustomerOptionValue.descriptions,qCustomerOptionValueDescription).fetch()
-			.where(qCustomerOption.merchantStore.id.eq(store.getId())
-			.and(qCustomerOptionDescription.language.id.eq(language.getId()))
-			.and(qCustomerOptionValueDescription.language.id.eq(language.getId())))
-			.orderBy(qCustomerOptionSet.sortOrder.asc());
+		
+		StringBuilder qs = new StringBuilder();
+		qs.append("select distinct cos from CustomerOptionSet as cos ");
+		qs.append("join fetch cos.pk.customerOption po ");
+		qs.append("join fetch cos.pk.customerOptionValue ov ");
+		qs.append("join fetch po.merchantStore pm ");
+		qs.append("left join fetch po.descriptions pop ");
+		qs.append("left join fetch ov.descriptions ovd ");
+		qs.append("where pm.id =:mid ");
+		qs.append("and pop.language.id =:lid ");
+		qs.append("and ovd.language.id =:lid ");
+		qs.append("order by cos.sortOrder asc");
+		
+		String hql = qs.toString();
+		Query q = super.getEntityManager().createQuery(hql);
+
+
+    	q.setParameter("mid", store.getId());
+    	q.setParameter("lid", language.getId());
+		
+		
+		//query.from(qCustomerOptionSet)
+		//	.join(qCustomerOptionSet.pk.customerOption,qCustomerOption).fetch()
+		//	.join(qCustomerOption.merchantStore,qMerchantStore).fetch()
+		//	.join(qCustomerOptionSet.pk.customerOptionValue,qCustomerOptionValue).fetch()
+		//	.where(qMerchantStore.id.eq(store.getId()));
+			//.leftJoin(qCustomerOption.descriptions,qCustomerOptionDescription).fetch()
+			//.leftJoin(qCustomerOptionValue.descriptions,qCustomerOptionValueDescription).fetch()
+			
+			//.and(qCustomerOptionDescription.language.id.eq(language.getId()))
+			//.and(qCustomerOptionValueDescription.language.id.eq(language.getId())))
+			//.orderBy(qCustomerOptionSet.sortOrder.asc());
 
 		
-		return query.list(qCustomerOptionSet);
+		@SuppressWarnings("unchecked")
+		List<CustomerOptionSet> returnList = q.getResultList();
+		return returnList;
 	}
 	
 
