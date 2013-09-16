5865265668158b3d0122bead38c43a1dac7e6bc8
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-09-16 17:37:16 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@2179 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/business/customer/model/attribute/CustomerOptionSetId.java
+++ sm-core/src/main/java/com/salesmanager/core/business/customer/model/attribute/CustomerOptionSetId.java
@@ -35,6 +35,39 @@ public class CustomerOptionSetId implements Serializable {
 	public void setCustomerOptionValue(CustomerOptionValue customerOptionValue) {
 		this.customerOptionValue = customerOptionValue;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((customerOption == null) ? 0 : customerOption.hashCode());
+		result = prime
+				* result
+				+ ((customerOptionValue == null) ? 0 : customerOptionValue
+						.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CustomerOptionSetId other = (CustomerOptionSetId) obj;
+		if (customerOption == null) {
+			if (other.customerOption != null)
+				return false;
+		} else if (!customerOption.equals(other.customerOption))
+			return false;
+		if (customerOptionValue == null) {
+			if (other.customerOptionValue != null)
+				return false;
+		} else if (!customerOptionValue.equals(other.customerOptionValue))
+			return false;
+		return true;
+	}
 
 
 }
