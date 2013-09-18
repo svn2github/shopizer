00b15790e35346f02790c118f69ce89d7684d928
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-09-18 18:48:41 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@2191 e80ee2c7-76f3-4005-aeaf-98396743f455

---
+++ sm-shop/src/main/java/com/salesmanager/web/entity/catalog/PageProducts.java
@@ -0,0 +1,28 @@
+package com.salesmanager.web.entity.catalog;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class PageProducts implements Serializable {
+	
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int totalCount;
+	private List<Product> products;
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	public List<Product> getProducts() {
+		return products;
+	}
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+	public int getTotalCount() {
+		return totalCount;
+	}
+
+}
