d2d52c909de0785e326cb9228381341debed8d21
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-09-26 02:30:18 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@2245 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/business/catalog/product/service/ProductServiceImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/catalog/product/service/ProductServiceImpl.java
@@ -8,7 +8,6 @@ import java.util.Locale;
 import java.util.Set;
 
 import org.apache.commons.lang.Validate;
-import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
