2529c34790d498977722abf02efe48002bc6b61b
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-10-16 17:36:27 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@2345 e80ee2c7-76f3-4005-aeaf-98396743f455

---
+++ sm-shop/src/main/java/com/salesmanager/web/admin/entity/catalog/Keyword.java
@@ -0,0 +1,38 @@
+package com.salesmanager.web.admin.entity.catalog;
+
+import java.io.Serializable;
+
+/**
+ * Post keyword from the admin
+ * @author Carl Samson
+ *
+ */
+public class Keyword implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private long productId;
+	private String languageCode;
+	private String keyword;
+	public void setProductId(long productId) {
+		this.productId = productId;
+	}
+	public long getProductId() {
+		return productId;
+	}
+	public void setLanguageCode(String languageCode) {
+		this.languageCode = languageCode;
+	}
+	public String getLanguageCode() {
+		return languageCode;
+	}
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+	public String getKeyword() {
+		return keyword;
+	}
+
+}
