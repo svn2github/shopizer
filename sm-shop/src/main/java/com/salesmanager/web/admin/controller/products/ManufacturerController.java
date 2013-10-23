31434f5c19301468a86e194171d2daa15634fbc0
fbourgault <fbourgault@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-10-23 15:55:57 +0000

    added code in display/create/save methods
    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@2399 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-shop/src/main/java/com/salesmanager/web/admin/controller/products/ManufacturerController.java
+++ sm-shop/src/main/java/com/salesmanager/web/admin/controller/products/ManufacturerController.java
@@ -1,30 +1,74 @@
 package com.salesmanager.web.admin.controller.products;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Set;
 
+import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.salesmanager.core.business.catalog.product.model.description.ProductDescription;
+import com.salesmanager.core.business.catalog.product.model.image.ProductImage;
+import com.salesmanager.core.business.catalog.product.model.image.ProductImageDescription;
+import com.salesmanager.core.business.catalog.product.model.manufacturer.Manufacturer;
+import com.salesmanager.core.business.catalog.product.model.manufacturer.ManufacturerDescription;
+import com.salesmanager.core.business.catalog.product.service.manufacturer.ManufacturerService;
+import com.salesmanager.core.business.customer.model.Customer;
+import com.salesmanager.core.business.customer.model.CustomerCriteria;
+import com.salesmanager.core.business.customer.model.CustomerList;
+import com.salesmanager.core.business.merchant.model.MerchantStore;
+import com.salesmanager.core.business.reference.language.model.Language;
+import com.salesmanager.core.business.reference.language.service.LanguageService;
+import com.salesmanager.core.utils.CoreConfiguration;
 import com.salesmanager.core.utils.ajax.AjaxPageableResponse;
 import com.salesmanager.core.utils.ajax.AjaxResponse;
 import com.salesmanager.web.admin.controller.ControllerConstants;
+import com.salesmanager.web.admin.controller.customers.CustomerController;
 import com.salesmanager.web.admin.entity.web.Menu;
+import com.salesmanager.web.constants.Constants;
+import com.salesmanager.web.utils.LabelUtils;
 
 @Controller
 public class ManufacturerController {
 	
+	@Autowired
+	private LanguageService languageService;
 	
-	@Secured("PRODUCTS")
+	@Autowired
+	private ManufacturerService manufacturerService;
+	
+	@Autowired
+	LabelUtils messages;
+	
+	@Autowired
+	private CoreConfiguration configuration;
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(CustomerController.class);
+	
+	@Secured("MANUFACTURER")
 	@RequestMapping(value="/admin/catalogue/manufacturer/list.html", method=RequestMethod.GET)
 	public String getManufacturers(Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
 	
@@ -34,35 +78,213 @@ public class ManufacturerController {
 		return ControllerConstants.Tiles.Product.manufacturerList;
 	}
 	
-	@Secured("PRODUCTS")
+	@Secured("MANUFACTURER")
 	@RequestMapping(value="/admin/catalogue/manufacturer/create.html", method=RequestMethod.GET)
-	public String createManufacturer(Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		this.setMenu(model, request);
-
+	public String createManufacturer(  Model model,  HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		return ControllerConstants.Tiles.Product.manufacturerDetails;
+		return displayManufacturer(0l,model,request,response);		
 	}
 	
-	@Secured("PRODUCTS")
+	@Secured("MANUFACTURER")
 	@RequestMapping(value="/admin/catalogue/manufacturer/edit.html", method=RequestMethod.GET)
 	public String editManufacturer(@RequestParam("id") long manufacturerId, Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		this.setMenu(model, request);
 
+		return displayManufacturer(manufacturerId,model,request,response);
+	}
+	
+	private String displayManufacturer(Long manufacturerId, Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		//display menu
+		setMenu(model,request);
+		
+		List<Language> languages = languageService.getLanguages();
+		com.salesmanager.web.admin.entity.catalog.Manufacturer manufacturer = new com.salesmanager.web.admin.entity.catalog.Manufacturer();		
+		List<ManufacturerDescription> descriptions = new ArrayList<ManufacturerDescription>();
+		MerchantStore store = (MerchantStore)request.getAttribute(Constants.ADMIN_STORE);
+		
+		
+		if( manufacturerId!=0) {//edit mode
+
+//			System.out.println("\n\n *************  ManufacturerController Edit  **************\n\n");
+			
+		} else {	// Create mode
+//			System.out.println("\n\n *************  ManufacturerController Create  **************\n\n");
+
+			Manufacturer manufacturerTmp = new Manufacturer();
+			manufacturer.setManufacturer( manufacturerTmp );
+			ManufacturerDescription manufacturerDesc = new ManufacturerDescription();
+			descriptions.add(  manufacturerDesc );
+			manufacturer.setDescriptions(descriptions);
+		}
+
+		model.addAttribute("languages",languages);
+		model.addAttribute("manufacturer", manufacturer);
 		
 		return ControllerConstants.Tiles.Product.manufacturerDetails;
 	}
-	
-	@Secured("PRODUCTS")
-	@RequestMapping(value="/admin/catalogue/manufacturer/save.html", method=RequestMethod.GET)
-	public String saveManufacturer(Model model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+	@Secured("MANUFACTURER")   //  @Valid
+	@RequestMapping(value="/admin/catalogue/manufacturer/save.html", method=RequestMethod.POST)
+	public String saveManufacturer( @Valid @ModelAttribute("manufacturer") com.salesmanager.web.admin.entity.catalog.Manufacturer manufacturer, BindingResult result, Model model,  HttpServletRequest request, HttpServletResponse response, Locale locale) throws Exception {
+		
 		this.setMenu(model, request);
 		//save or edit a manufacturer
+		
+		MerchantStore store = (MerchantStore)request.getAttribute(Constants.ADMIN_STORE);
+		List<Language> languages = languageService.getLanguages();
+		
+		if(manufacturer.getDescriptions()!=null && manufacturer.getDescriptions().size()>0) {
+			
+			for(ManufacturerDescription description : manufacturer.getDescriptions()) {
+				
+				//validate Url Clicked
+				if ( description.getUrlClicked() != null && !description.getUrlClicked().toString().isEmpty()) {
+					try{
+						Integer.parseInt( description.getUrlClicked().toString() );
+						
+					} catch (Exception e) {
+						
+						ObjectError error = new ObjectError("descriptions[${counter.index}].urlClicked","URL Clicked must be a number");
+						result.addError(error);
+					}
+				}
+			}
+		}
+		
+		//validate image
+//		if(manufacturer.getImage()!=null && !manufacturer.getImage().isEmpty()) {
+//			
+//			try {
+//				
+//				String maxHeight = configuration.getProperty("PRODUCT_IMAGE_MAX_HEIGHT_SIZE");
+//				String maxWidth = configuration.getProperty("PRODUCT_IMAGE_MAX_WIDTH_SIZE");
+//				String maxSize = configuration.getProperty("PRODUCT_IMAGE_MAX_SIZE");
+//				
+//				
+//				BufferedImage image = ImageIO.read(manufacturer.getImage().getInputStream());
+//				
+//				
+//				if(!StringUtils.isBlank(maxHeight)) {
+//					
+//					int maxImageHeight = Integer.parseInt(maxHeight);
+//					if(image.getHeight()>maxImageHeight) {
+//						ObjectError error = new ObjectError("image",messages.getMessage("message.image.height", locale) + " {"+maxHeight+"}");
+//						result.addError(error);
+//					}
+//					
+//				}
+//				
+//				if(!StringUtils.isBlank(maxWidth)) {
+//					
+//					int maxImageWidth = Integer.parseInt(maxWidth);
+//					if(image.getWidth()>maxImageWidth) {
+//						ObjectError error = new ObjectError("image",messages.getMessage("message.image.width", locale) + " {"+maxWidth+"}");
+//						result.addError(error);
+//					}
+//					
+//				}
+//				
+//				if(!StringUtils.isBlank(maxSize)) {
+//					
+//					int maxImageSize = Integer.parseInt(maxSize);
+//					if(manufacturer.getImage().getSize()>maxImageSize) {
+//						ObjectError error = new ObjectError("image",messages.getMessage("message.image.size", locale) + " {"+maxSize+"}");
+//						result.addError(error);
+//					}
+//					
+//				}
+//				
+//			} catch (Exception e) {
+//				LOGGER.error("Cannot validate product image", e);
+//			}
+//
+//		}
+		
+ 		if (result.hasErrors()) {
+			model.addAttribute("languages",languages);
+			return ControllerConstants.Tiles.Product.manufacturerDetails;
+		}		
+		
+		
+		if ( manufacturer.getManufacturer().getId() !=null && manufacturer.getManufacturer().getId()  > 0 ){
+			
+			Manufacturer dbManufacturer = new Manufacturer();
+			
+			return ControllerConstants.Tiles.Product.manufacturerDetails;
+			
+		} else {			
+		
+	
+			Set<ManufacturerDescription> descriptions = new HashSet<ManufacturerDescription>();	 
+			
+			Manufacturer newManufacturer = new Manufacturer();
+			newManufacturer.setMerchantStore(manufacturer.getManufacturer().getMerchantStore() );
+			newManufacturer.setOrder( manufacturer.getOrder() );
+			
+			if(manufacturer.getDescriptions()!=null && manufacturer.getDescriptions().size()>0) {
+			
+				for(ManufacturerDescription description : manufacturer.getDescriptions()) {
+					description.setManufacturer(newManufacturer);
+					descriptions.add(description);
+				}
+			}	
+	
+			newManufacturer.setDescriptions(descriptions);
+			newManufacturer.setMerchantStore(store);
+			manufacturerService.saveOrUpdate(newManufacturer);
+			manufacturer.setManufacturer( newManufacturer );  
+		}
+		
+		/*
+		 if(product.getImage()!=null && !product.getImage().isEmpty()) {			
 
+			
+			String imageName = product.getImage().getOriginalFilename();		
+
+			ProductImage productImage = new ProductImage();
+			productImage.setDefaultImage(true);
+			productImage.setImage(product.getImage().getInputStream());
+			productImage.setProductImage(imageName);
+			
+			
+			List<ProductImageDescription> imagesDescriptions = new ArrayList<ProductImageDescription>();
+
+			for(Language l : languages) {
+				
+				ProductImageDescription imageDescription = new ProductImageDescription();
+				imageDescription.setName(imageName);
+				imageDescription.setLanguage(l);
+				imageDescription.setProductImage(productImage);
+				imagesDescriptions.add(imageDescription);
+				
+			}
+			
+			productImage.setDescriptions(imagesDescriptions);
+			productImage.setProduct(newProduct);
+			
+			newProduct.getImages().add(productImage);
+			
+			productService.saveOrUpdate(newProduct);
+			
+			//product displayed
+			product.setProductImage(productImage);
+			
+			
+		} else {
+			
+			productService.saveOrUpdate(newProduct);
+			
+		}
+		 */
+		
+		model.addAttribute("manufacturer", manufacturer);		
+		model.addAttribute("languages",languages);
+		model.addAttribute("success","success");
 		
 		return ControllerConstants.Tiles.Product.manufacturerDetails;
 	}
 	
-	@Secured("PRODUCTS")
+	@Secured("MANUFACTURER")
 	@RequestMapping(value="/admin/catalogue/manufacturer/paging.html", method=RequestMethod.POST, produces="application/json")
 	public @ResponseBody String pageManufacturers(HttpServletRequest request, HttpServletResponse response) {
 
@@ -76,7 +298,7 @@ public class ManufacturerController {
 		
 	}
 	
-	@Secured("PRODUCTS")
+	@Secured("MANUFACTURER")
 	@RequestMapping(value="/admin/catalogue/manufacturer/remove.html", method=RequestMethod.POST, produces="application/json")
 	public @ResponseBody String deleteManufacturer(HttpServletRequest request, HttpServletResponse response, Locale locale) {
 		String sid = request.getParameter("id");
