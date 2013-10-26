49fe87df76bb5d3644f19a83ace38b59df9f6f23
fbourgault <fbourgault@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-10-26 17:51:17 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@2427 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/business/catalog/product/service/manufacturer/ManufacturerService.java
+++ sm-core/src/main/java/com/salesmanager/core/business/catalog/product/service/manufacturer/ManufacturerService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.salesmanager.core.business.catalog.product.model.Product;
 import com.salesmanager.core.business.catalog.product.model.manufacturer.Manufacturer;
+import com.salesmanager.core.business.catalog.product.model.manufacturer.ManufacturerDescription;
 import com.salesmanager.core.business.generic.exception.ServiceException;
 import com.salesmanager.core.business.generic.service.SalesManagerEntityService;
 import com.salesmanager.core.business.merchant.model.MerchantStore;
@@ -17,5 +18,7 @@ public interface ManufacturerService extends SalesManagerEntityService<Long, Man
 	List<Manufacturer> listByStore(MerchantStore store) throws ServiceException;
 
 	void saveOrUpdate(Manufacturer manufacturer) throws ServiceException;
+	
+	void addManufacturerDescription(Manufacturer manufacturer, ManufacturerDescription description) throws ServiceException;
 	
 }
--- sm-core/src/main/java/com/salesmanager/core/business/catalog/product/service/manufacturer/ManufacturerServiceImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/catalog/product/service/manufacturer/ManufacturerServiceImpl.java
@@ -1,6 +1,8 @@
 package com.salesmanager.core.business.catalog.product.service.manufacturer;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,18 +10,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.salesmanager.core.business.catalog.product.dao.manufacturer.ManufacturerDao;
+import com.salesmanager.core.business.catalog.product.model.Product;
+import com.salesmanager.core.business.catalog.product.model.description.ProductDescription;
 import com.salesmanager.core.business.catalog.product.model.manufacturer.Manufacturer;
+import com.salesmanager.core.business.catalog.product.model.manufacturer.ManufacturerDescription;
 import com.salesmanager.core.business.customer.model.Customer;
 import com.salesmanager.core.business.customer.service.CustomerServiceImpl;
 import com.salesmanager.core.business.generic.exception.ServiceException;
 import com.salesmanager.core.business.generic.service.SalesManagerEntityServiceImpl;
 import com.salesmanager.core.business.merchant.model.MerchantStore;
 import com.salesmanager.core.business.reference.language.model.Language;
+import com.salesmanager.core.business.search.service.SearchService;
+
+
 
 @Service("manufacturerService")
 public class ManufacturerServiceImpl extends
 		SalesManagerEntityServiceImpl<Long, Manufacturer> implements ManufacturerService {
 
+	@Autowired
+	SearchService searchService;
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(CustomerServiceImpl.class);
 	private ManufacturerDao manufacturerDao;
 	
@@ -41,17 +52,38 @@ public class ManufacturerServiceImpl extends
 		return manufacturerDao.listByStore(store);
 	}
 
+	@Override
+	public void addManufacturerDescription(Manufacturer manufacturer, ManufacturerDescription description)
+			throws ServiceException {
+		
+		
+		if(manufacturer.getDescriptions()==null) {
+			manufacturer.setDescriptions(new HashSet<ManufacturerDescription>());
+		}
+		
+		manufacturer.getDescriptions().add(description);
+		description.setManufacturer(manufacturer);
+		update(manufacturer);
+//		searchService.index(manufacturer.getMerchantStore(), manufacturer);
+	}
+	
 	@Override	
 	public void saveOrUpdate(Manufacturer manufacturer) throws ServiceException {
 
 		LOGGER.debug("Creating Manufacturer");
 		
 		if(manufacturer.getId()!=null && manufacturer.getId()>0) {
-//			super.update(manufacturer);   // commented for now, get constraint error( update manuf_description code missing)  
+			super.update(manufacturer);   // commented for now, get constraint error( update manuf_description code missing)  
 		} else {			
 		
+			Set<ManufacturerDescription> manufacturerDescriptions = manufacturer.getDescriptions();
+			manufacturer.setDescriptions(null);
+			
 			super.create(manufacturer);
 
+			for(ManufacturerDescription manufacturerDescription : manufacturerDescriptions) {
+				addManufacturerDescription(manufacturer,manufacturerDescription);
+			}
 		}
 	}
 }
