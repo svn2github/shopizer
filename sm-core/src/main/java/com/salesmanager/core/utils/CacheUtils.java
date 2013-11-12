2fb73d29402fef286e89d6899bd7dd4b9b21a9ec
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-11-12 00:55:25 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@2550 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/utils/CacheUtils.java
+++ sm-core/src/main/java/com/salesmanager/core/utils/CacheUtils.java
@@ -26,50 +26,13 @@ public class CacheUtils {
 	public final static String REFERENCE_CACHE = "REF";
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(CacheUtils.class);
-	
-	//private static CacheUtils cacheUtils = null;
 
-	//private final static String repositoryFileName = "cms/infinispan_configuration.xml";
-	
 	private final static String KEY_DELIMITER = "_";
 	
-	//private Cache<Object, Object> localCache = null;
-	
 
-	
-	
-/*	private CacheUtils() {
-		
-		try {
-
-			   
-			   localCache =  new DefaultCacheManager(repositoryFileName).getCache("Cache"); 
-
-			
-			
-		} catch (Exception e) {
-			LOGGER.error("Error loading cache singletons", e);
-		}
-		
-	}*/
-	
-/*	public static CacheUtils getInstance() {
-		
-		if(cacheUtils==null) {
-			cacheUtils = new CacheUtils();
-	
-		}
-		
-		return cacheUtils;
-		
-	}*/
-	
-
-	
 
 	public void putInCache(Object object, String keyName) throws Exception {
 
-		//localCache.put(keyName, object);
 		cache.put(keyName, object);
 		
 	}
@@ -77,7 +40,6 @@ public class CacheUtils {
 
 	public Object getFromCache(String keyName) throws Exception {
 
-		 //return localCache.get(keyName);
 		ValueWrapper vw = cache.get(keyName);
 		if(vw!=null) {
 			return vw.get();
@@ -110,31 +72,8 @@ public class CacheUtils {
 				} catch (Exception e) {
 					LOGGER.equals("key " + key + " cannot be converted to a String or parsed");
 				}  
-
 		  }
-		
-/*		Set<Object> keys = localCache.keySet();
-		List<String> returnKeys = new ArrayList<String>();
-		for(Object key : keys) {
-			try {
-				String sKey = (String)key;
-				
-				// a key should be <storeId>_<rest of the key>
-				int delimiterPosition = sKey.indexOf(KEY_DELIMITER);
-				
-				if(delimiterPosition>0 && Character.isDigit(sKey.charAt(0))) {
-				
-					String keyRemaining = sKey.substring(delimiterPosition+1);
-					returnKeys.add(keyRemaining);
-				
-				}
 
-			} catch (Exception e) {
-				LOGGER.equals("key " + key + " cannot be converted to a String or parsed");
-			}
-
-		}*/
-		
 		return returnKeys;
 	}
 	
@@ -143,8 +82,29 @@ public class CacheUtils {
 	}
 	
 	public void removeFromCache(String keyName) throws Exception {
-		//localCache.remove(keyName);
 		cache.evict(keyName);
+	}
+	
+	public void removeAllFromCache(MerchantStore store) throws Exception {
+		  net.sf.ehcache.Cache cacheImpl = (net.sf.ehcache.Cache) cache.getNativeCache();
+		  for (Object key: cacheImpl.getKeys()) {
+				try {
+					String sKey = (String)key;
+					
+					// a key should be <storeId>_<rest of the key>
+					int delimiterPosition = sKey.indexOf(KEY_DELIMITER);
+					
+					if(delimiterPosition>0 && Character.isDigit(sKey.charAt(0))) {
+					
+
+						cache.evict(key);
+					
+					}
+
+				} catch (Exception e) {
+					LOGGER.equals("key " + key + " cannot be converted to a String or parsed");
+				}  
+		  }
 	}
 	
 
