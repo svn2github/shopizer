6a154315ebe98b4dfa9bcc39f87cd5b54dfbb3cd
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-11-14 17:58:02 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@2567 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/main/java/com/salesmanager/core/utils/DataPopulator.java
+++ sm-core/src/main/java/com/salesmanager/core/utils/DataPopulator.java
@@ -3,6 +3,7 @@
  */
 package com.salesmanager.core.utils;
 
+import com.salesmanager.core.business.generic.exception.ServiceException;
 import com.salesmanager.core.business.merchant.model.MerchantStore;
 import com.salesmanager.core.business.reference.language.model.Language;
 
@@ -14,6 +15,6 @@ public interface DataPopulator<Source,Target>
 {
 
 
-    public Target populateFromEntity(Source source,Target target, MerchantStore store, Language language);
-    public Source populateToEntity(Source source, Target target, MerchantStore store);
+    public Target populateFromEntity(Source source,Target target, MerchantStore store, Language language) throws ServiceException;
+    public Source populateToEntity(Source target, Target source, MerchantStore store) throws ServiceException;
 }
