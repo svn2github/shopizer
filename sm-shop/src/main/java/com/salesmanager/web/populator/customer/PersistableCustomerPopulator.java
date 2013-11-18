f120299e6c5ee92574944334eba7cc10b8142ea5
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-11-18 20:18:42 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@2643 e80ee2c7-76f3-4005-aeaf-98396743f455

---
+++ sm-shop/src/main/java/com/salesmanager/web/populator/customer/PersistableCustomerPopulator.java
@@ -0,0 +1,76 @@
+package com.salesmanager.web.populator.customer;
+
+import com.salesmanager.core.business.customer.model.Customer;
+import com.salesmanager.core.business.generic.exception.ConversionException;
+import com.salesmanager.core.business.merchant.model.MerchantStore;
+import com.salesmanager.core.business.reference.language.model.Language;
+import com.salesmanager.core.utils.AbstractDataPopulator;
+import com.salesmanager.web.entity.customer.Address;
+import com.salesmanager.web.entity.customer.PersistableCustomer;
+
+public class PersistableCustomerPopulator extends
+		AbstractDataPopulator<Customer, PersistableCustomer> {
+
+	@Override
+	public PersistableCustomer populate(Customer source,
+			PersistableCustomer target, MerchantStore store, Language language)
+			throws ConversionException {
+
+		
+		try {
+
+			
+			if(source.getBilling()!=null) {
+				Address address = new Address();
+				address.setCity(source.getBilling().getCity());
+				address.setCompany(source.getBilling().getCompany());
+				address.setName(source.getBilling().getName());
+				address.setPostalCode(source.getBilling().getPostalCode());
+				if(source.getBilling().getCountry()!=null) {
+					address.setCountry(source.getBilling().getCountry().getIsoCode());
+				}
+				if(source.getBilling().getZone()!=null) {
+					address.setZone(source.getBilling().getZone().getCode());
+				}
+				
+				target.setBilling(address);
+			}
+			
+			if(source.getDelivery()!=null) {
+				Address address = new Address();
+				address.setCity(source.getDelivery().getCity());
+				address.setCompany(source.getDelivery().getCompany());
+				address.setName(source.getDelivery().getName());
+				address.setPostalCode(source.getDelivery().getPostalCode());
+				if(source.getDelivery().getCountry()!=null) {
+					address.setCountry(source.getDelivery().getCountry().getIsoCode());
+				}
+				if(source.getDelivery().getZone()!=null) {
+					address.setZone(source.getDelivery().getZone().getCode());
+				}
+				
+				target.setDelivery(address);
+			}
+			
+			target.setEmailAddress(source.getEmailAddress());
+			target.setGender(source.getGender().name());
+			target.setLanguage(source.getDefaultLanguage().getCode());
+			target.setUserName(source.getNick());
+			
+			
+			
+		} catch (Exception e) {
+			throw new ConversionException(e);
+		}
+			
+		return null;
+		
+	}
+
+	@Override
+	protected PersistableCustomer createTarget() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
