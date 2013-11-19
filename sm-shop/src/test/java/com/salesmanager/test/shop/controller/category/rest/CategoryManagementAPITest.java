2beaa459531ba38abb21a54c5c3ec59fc0b7da28
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2013-11-19 03:55:27 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@2644 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-shop/src/test/java/com/salesmanager/test/shop/controller/category/rest/CategoryManagementAPITest.java
+++ sm-shop/src/test/java/com/salesmanager/test/shop/controller/category/rest/CategoryManagementAPITest.java
@@ -1,5 +1,10 @@
 package com.salesmanager.test.shop.controller.category.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ObjectWriter;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.http.HttpEntity;
@@ -11,8 +16,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.codec.Base64;
 import org.springframework.web.client.RestTemplate;
 
-import com.salesmanager.web.entity.catalog.Category;
 import com.salesmanager.web.entity.catalog.Product;
+import com.salesmanager.web.entity.catalog.rest.category.Category;
+import com.salesmanager.web.entity.catalog.rest.category.CategoryDescription;
+import com.salesmanager.web.entity.catalog.rest.category.PersistableCategory;
+import com.salesmanager.web.entity.catalog.rest.category.ReadableCategory;
 
 public class CategoryManagementAPITest {
 	
@@ -23,20 +31,22 @@ public class CategoryManagementAPITest {
 	private Long testProductID;
 
 	@Test
-	@Ignore
+	//@Ignore
 	public void categoryRESTTest() throws Exception {
+		
+		//getCategory();
 		
 		//create customer
 		postCategory();
 		
 		//get All customers
-		getCategories();
+		//getCategories();
 		
 		//update customer
 		//putCategory();
 		
 		//delete customer
-		deleteCategory();
+		//deleteCategory();
 		
 	}
 	
@@ -50,7 +60,21 @@ public class CategoryManagementAPITest {
 		return headers;
 	}
 	
-	public void getCategories() throws Exception {
+	public void getCategory() throws Exception {
+		restTemplate = new RestTemplate();
+		
+		HttpEntity<String> httpEntity = new HttpEntity<String>(getHeader());
+		
+		ResponseEntity<ReadableCategory> response = restTemplate.exchange("http://localhost:8080/sm-shop/shop/services/public/category/DEFAULT/en/4", HttpMethod.GET, httpEntity, ReadableCategory.class);
+		
+		if(response.getStatusCode() != HttpStatus.OK){
+			throw new Exception();
+		}else{
+			System.out.println(response.getBody() + " Category record found.");
+		}
+	}
+	
+/*	public void getCategories() throws Exception {
 		restTemplate = new RestTemplate();
 		
 		HttpEntity<String> httpEntity = new HttpEntity<String>(getHeader());
@@ -62,9 +86,9 @@ public class CategoryManagementAPITest {
 		}else{
 			System.out.println(response.getBody().length + " Category records found.");
 		}
-	}
+	}*/
 	
-	public void putCategory() throws Exception {
+/*	public void putCategory() throws Exception {
 		restTemplate = new RestTemplate();
 	
 		String jsonString = ("{"+
@@ -88,37 +112,59 @@ public class CategoryManagementAPITest {
 		
 		HttpEntity<String> entity = new HttpEntity<String>(jsonString, getHeader());
 		
-		restTemplate.put("http://localhost:8080/sm-shop/shop/services/rest/category/DEFAULT/en/"+testCategoryID, entity);
+		restTemplate.put("http://localhost:8080/sm-shop/shop/services/private/category/DEFAULT"+testCategoryID, entity);
 		System.out.println("Category "+testCategoryID+" Updated.");
-	}
+	}*/
 	
 	public void postCategory() throws Exception {
 		restTemplate = new RestTemplate();
 		
-		String jsonString = ("{"+
+/*		String jsonString = ("{"+
 				   " \"sortOrder\": \"0\","+
 				   " \"depth\": \"2\","+
 				   " \"code\": \"movie"+Math.random()+"\","+
 				   " \"lineage\": \"/movie\","+
 				   " \"categoryStatus\": \"true\","+
 				   " \"visible\": \"true\","+
-				   " \"categories\": [{"+
-				   "     \"code\":\"music\""+
-				   " },{"+
-				   "     \"code\":\"music123\""+
-				   " }],"+
 				   " \"descriptions\": [{"+
 				   "     \"metatagTitle\": \"Hollywood Movie\","+
 				   "     \"name\":\"Hollywood\""+
 				   " }],"+
 				   " \"categoryImage\": \"image.jpg\""+
-				"}");
+				"}");*/
 		
-		HttpEntity<String> entity = new HttpEntity<String>(jsonString, getHeader());
+		PersistableCategory newCategory = new PersistableCategory();
+		newCategory.setCode("javascript");
+		newCategory.setSortOrder(1);
+		newCategory.setVisible(true);
+		
+		Category parent = new Category();
+		parent.setId(4L);
+		
+		newCategory.setParent(parent);
+		
+		CategoryDescription description = new CategoryDescription();
+		description.setLanguage("en");
+		description.setName("Javascript");
+		description.setFriendlyUrl("javascript");
+		description.setTitle("Javascript");
+		
+		List<CategoryDescription> descriptions = new ArrayList<CategoryDescription>();
+		descriptions.add(description);
+		
+		newCategory.setDescriptions(descriptions);
+		
+		ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		String json = writer.writeValueAsString(newCategory);
+		
+		System.out.println(json);
+		
+		
+		HttpEntity<String> entity = new HttpEntity<String>(json, getHeader());
 
-		ResponseEntity response = restTemplate.postForEntity("http://localhost:8080/sm-shop/shop/services/rest/category/DEFAULT/en", entity, Category.class);
+		ResponseEntity response = restTemplate.postForEntity("http://localhost:8080/sm-shop/shop/services/private/category/DEFAULT", entity, PersistableCategory.class);
 
-		Category cat = (Category) response.getBody();
+		PersistableCategory cat = (PersistableCategory) response.getBody();
 		System.out.println("New Category ID : " + cat.getId());
 		testCategoryID = cat.getId();
 	}
