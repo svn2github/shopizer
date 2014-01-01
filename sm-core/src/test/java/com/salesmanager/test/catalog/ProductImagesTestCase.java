fc8c754841d9b10cc3e66b55ad0c7afd8bc4c8a5
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2014-01-01 22:22:56 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@2862 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-core/src/test/java/com/salesmanager/test/catalog/ProductImagesTestCase.java
+++ sm-core/src/test/java/com/salesmanager/test/catalog/ProductImagesTestCase.java
@@ -1,6 +1,5 @@
 package com.salesmanager.test.catalog;
 
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -14,8 +13,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.imageio.ImageIO;
-
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -26,6 +23,7 @@ import com.salesmanager.core.business.catalog.category.model.CategoryDescription
 import com.salesmanager.core.business.catalog.product.model.Product;
 import com.salesmanager.core.business.catalog.product.model.availability.ProductAvailability;
 import com.salesmanager.core.business.catalog.product.model.description.ProductDescription;
+import com.salesmanager.core.business.catalog.product.model.file.ProductImageSize;
 import com.salesmanager.core.business.catalog.product.model.image.ProductImage;
 import com.salesmanager.core.business.catalog.product.model.manufacturer.Manufacturer;
 import com.salesmanager.core.business.catalog.product.model.manufacturer.ManufacturerDescription;
@@ -48,70 +46,7 @@ public class ProductImagesTestCase extends AbstractSalesManagerCoreTestCase {
 	
 	@Autowired
 	private ContentService contentService;
-	
-	
-/*	@Test
-	public void testCache() throws ServiceException, FileNotFoundException, IOException {
-		
-		
-        //Cache cache = new DefaultCacheManager("cms/infinispan_configuration.xml").getCache();
-        
-		 Cache cache = new DefaultCacheManager("cms/infinispan_configuration.xml").getCache("StoreRepository");
-		 cache.getCacheConfiguration().invocationBatching().enabled();
-    
-		 TreeCacheFactory f = new TreeCacheFactory();
-    
-		 TreeCache treeCache = f.createTreeCache(cache);
-		 
-		 treeCache.start();
-		 
 
-		 
-		 
-	        final File file1 = new File( "c:/doc/carl/Merchant.jpg" );
-
-	        if ( !file1.exists() || !file1.canRead() )
-	        {
-	            throw new ServiceException( "Can't read" + file1.getAbsolutePath() );
-	        }
-
-	        final byte[] is = IOUtils.toByteArray( new FileInputStream( file1 ) );
-	        final ByteArrayInputStream inputStream = new ByteArrayInputStream( is );
-	        final ImageContentFile cmsContentImage = new ImageContentFile();
-	        cmsContentImage.setFileName( file1.getName() );
-	        cmsContentImage.setFile( inputStream );
-	        cmsContentImage.setFileContentType(FileContentType.PRODUCT);
-	        
-
-            //Node<String, Object> productNode =  merchantNode.getChild(productFqn);
-	        Fqn contentFilesFqn = Fqn.fromString("/product-merchant-DEFAULT/TB12345"); 
-            Node<String, Object> productNode = treeCache.getRoot().getChild(contentFilesFqn);
-            
-            
-            
-            if(productNode==null) {
-            	//merchantNode.addChild(productFqn);
-            	
-            	treeCache.getRoot().addChild(contentFilesFqn);
-            	productNode = treeCache.getRoot().getChild(contentFilesFqn);
-            }
-		 
-            ByteArrayOutputStream output = new ByteArrayOutputStream();
-            IOUtils.copy( inputStream, output );
-		 
-            productNode.put(file1.getName(), output.toByteArray());
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-        
-
-	}
-	*/
-	
 	@Test
 	public void testCreateProductImage() throws ServiceException, FileNotFoundException, IOException {
 		
@@ -217,7 +152,7 @@ public class ProductImagesTestCase extends AbstractSalesManagerCoreTestCase {
    
         
         
-        final File file1 = new File( "/Users/csamson777/Documents/csti/JAP-LETTER-2.jpg" );
+        final File file1 = new File( "/Users/csamson777/Documents/workspace2/files/images/watch.jpg" );
 
         if ( !file1.exists() || !file1.canRead() )
         {
@@ -231,10 +166,6 @@ public class ProductImagesTestCase extends AbstractSalesManagerCoreTestCase {
         cmsContentImage.setFile( inputStream );
         cmsContentImage.setFileContentType(FileContentType.PRODUCT);
         
-        //BufferedImage bufferedImage = ImageIO.read(inputStream);
-		//cmsContentImage.setBufferedImage(bufferedImage);
-
-	    //final List<ProductImage> contentImagesList=new ArrayList<ProductImage>();
 
         ProductImage productImage = new ProductImage();
         productImage.setProductImage(file1.getName());
@@ -246,49 +177,34 @@ public class ProductImagesTestCase extends AbstractSalesManagerCoreTestCase {
         //get productImage
         productImage = productImageService.getById(productImage.getId());
         
-        //get physical image
-        OutputContentFile contentFile = productImageService.getProductImage(store.getCode(), product.getSku(), productImage.getProductImage());
+        //get physical small image
+        OutputContentFile contentFile = productImageService.getProductImage(store.getCode(), product.getSku(), productImage.getProductImage(), ProductImageSize.SMALL);
         
         Assert.assertNotNull(contentFile);
         
         //print image
-   	 	OutputStream outputStream = new FileOutputStream ("/Users/csamson777/Documents/workspace2/csti" + contentFile.getFileName()); 
+   	 	OutputStream outputStream = new FileOutputStream ("/Users/csamson777/Documents/workspace2/files/images/small_" + contentFile.getFileName()); 
 
    	 	ByteArrayOutputStream baos =  contentFile.getFile();
+   	 	baos.writeTo(outputStream);
+   	 	
+   	 	
+   	 	//get physical original image
+        contentFile = productImageService.getProductImage(store.getCode(), product.getSku(), productImage.getProductImage(), ProductImageSize.LARGE);
+        
+        Assert.assertNotNull(contentFile);
+        
+        //print image
+   	 	outputStream = new FileOutputStream ("/Users/csamson777/Documents/workspace2/files/images/large_" + contentFile.getFileName()); 
+
+   	 	baos =  contentFile.getFile();
    	 	baos.writeTo(outputStream);
    	 	
    	 	//remove productImage
    	 	productImageService.removeProductImage(productImage);
    	 	
    	 	
-        final File file2 = new File( "c:/doc/carl/IA.jpg" );
 
-        if ( !file2.exists() || !file2.canRead() )
-        {
-            throw new ServiceException( "Can't read" + file2.getAbsolutePath() );
-        }
-
-        final byte[] is2 = IOUtils.toByteArray( new FileInputStream( file2 ) );
-        final ByteArrayInputStream inputStream2 = new ByteArrayInputStream( is2 );
-        final ImageContentFile cmsContentImage2 = new ImageContentFile();
-        cmsContentImage2.setFileName( file2.getName() );
-        cmsContentImage2.setFile( inputStream2 );
-        cmsContentImage2.setFileContentType(FileContentType.PRODUCT);
-        
-        BufferedImage bufferedImage2 = ImageIO.read(inputStream2);
-		cmsContentImage2.setBufferedImage(bufferedImage2);
-
-	    //final List<ProductImage> contentImagesList=new ArrayList<ProductImage>();
-
-        ProductImage productImage2 = new ProductImage();
-        productImage2.setProductImage(file2.getName());
-        productImage2.setProduct(product);
-
-        
-        productImageService.addProductImage(product, productImage2, cmsContentImage2);
-        
-   	 	//remove productImage
-   	 	productImageService.removeProductImage(productImage2);
 	    
 	    
 
