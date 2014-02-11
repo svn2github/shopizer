a1929f4e9ae79a3bd99c95ddabe44278327d0ef4
uawasthi <uawasthi@e80ee2c7-76f3-4005-aeaf-98396743f455>
2014-02-11 17:04:03 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@2967 e80ee2c7-76f3-4005-aeaf-98396743f455

---
+++ sm-shop/src/main/java/com/salesmanager/web/shop/constraints/impl/FieldMatchValidator.java
@@ -0,0 +1,47 @@
+package com.salesmanager.web.shop.constraints.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.salesmanager.web.shop.constraints.FieldMatch;
+import com.salesmanager.web.shop.validation.BeanUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+
+public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Object>
+{
+    
+    private static final Logger LOG=LoggerFactory.getLogger(FieldMatchValidator.class);
+    private String firstFieldName;
+    private String secondFieldName;
+    private BeanUtils beanUtils;
+
+    @Override
+    public void initialize(final FieldMatch constraintAnnotation)
+    {
+        this.firstFieldName = constraintAnnotation.first();
+        this.secondFieldName = constraintAnnotation.second();
+        this.beanUtils=BeanUtils.newInstance();
+    }
+
+    @SuppressWarnings( "nls" )
+    @Override
+    public boolean isValid(final Object value, final ConstraintValidatorContext context)
+    {
+        try
+        {
+            final Object firstObj = this.beanUtils.getPropertyValue(value, this.firstFieldName);
+            final Object secondObj = this.beanUtils.getPropertyValue(value, this.secondFieldName);
+            return firstObj == null && secondObj == null || firstObj != null && firstObj.equals(secondObj);
+        }
+        catch (final Exception ex)
+        {
+            LOG.info( "Error while getting values from object", ex );
+            return false;
+           
+        }
+       
+    }
+}
