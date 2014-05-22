34ee4bbd5dffb6e06d1009c50a69996ecce6faa5
csamson777 <csamson777@e80ee2c7-76f3-4005-aeaf-98396743f455>
2014-05-22 15:49:06 +0000

    git-svn-id: http://svn.code.sf.net/p/shopizer/code/shopizer-portlet@3144 e80ee2c7-76f3-4005-aeaf-98396743f455

--- sm-shop/src/main/java/com/salesmanager/web/shop/controller/customer/CustomerOrdersController.java
+++ sm-shop/src/main/java/com/salesmanager/web/shop/controller/customer/CustomerOrdersController.java
@@ -30,8 +30,8 @@ import com.salesmanager.web.entity.order.ReadableOrderList;
 import com.salesmanager.web.shop.controller.AbstractController;
 import com.salesmanager.web.shop.controller.ControllerConstants;
 import com.salesmanager.web.shop.controller.customer.facade.CustomerFacade;
-import com.salesmanager.web.shop.controller.data.paging.PaginationData;
 import com.salesmanager.web.shop.controller.order.facade.OrderFacade;
+import com.salesmanager.web.shop.model.paging.PaginationData;
 
 @Controller
 @RequestMapping(Constants.SHOP_URI + "/customer")
@@ -77,98 +77,44 @@ public class CustomerOrdersController extends AbstractController {
         
         PaginationData paginaionData=createPaginaionData(page,Constants.MAX_ORDERS_PAGE);
         ReadableOrderList readable= orderFacade.getReadableOrderList(store, customer, (paginaionData.getOffset() -1),paginaionData.getPageSize(), language);
-        model.addAttribute( "paginationData", calculatePaginaionData(paginaionData,readable.getTotal()));
+        
         model.addAttribute( "customerOrders", readable);
+        if(readable!=null) {
+        	model.addAttribute( "paginationData", calculatePaginaionData(paginaionData,readable.getTotal()));
+        } else {
+        	model.addAttribute( "paginationData", null);
+        }
+        
+        
+        
         StringBuilder template = new StringBuilder().append(ControllerConstants.Tiles.Customer.customerOrders).append(".").append(store.getStoreTemplate());
         return template.toString();
 	}
 	
-	//TODO move to REST services
+
 	@PreAuthorize("hasRole('AUTH_CUSTOMER')")
-	@RequestMapping( value="/{store}/orders/", method=RequestMethod.GET)
-	@ResponseStatus(HttpStatus.ACCEPTED)
-	@ResponseBody
-	public ReadableOrderList listOrders(@PathVariable final String store, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
-		MerchantStore merchantStore = (MerchantStore)request.getAttribute(Constants.MERCHANT_STORE);
-		if(merchantStore!=null) {
-			if(!merchantStore.getCode().equals(store)) {
-				merchantStore = null;
-			}
-		}
-		
-		if(merchantStore== null) {
-			merchantStore = merchantStoreService.getByCode(store);
-		}
-		
-		if(merchantStore==null) {
-			LOGGER.error("Merchant store is null for code " + store);
-			response.sendError(503, "Merchant store is null for code " + store);
-			return null;
-		}
-		
+    @RequestMapping(value="/order.html", method={RequestMethod.GET,RequestMethod.POST})
+    public String order(final Model model,final HttpServletRequest request,@RequestParam(value = "orderId" ,required=true) final String orderId) throws Exception{
+        
+		MerchantStore store = getSessionAttribute(Constants.MERCHANT_STORE, request);
+		if(StringUtils.isBlank( orderId )){
+        	LOGGER.error( "Order Id can not be null or empty" );
+        }
+        LOGGER.info( "Fetching order details for Id " +orderId);
+        
+        //get order id
+        
+        
+        //check if order belongs to customer logged in
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		Customer customer = null;
     	if(auth != null &&
         		 request.isUserInRole("AUTH_CUSTOMER")) {
-    		customer = customerFacade.getCustomerByUserName(auth.getName(), merchantStore);
+    		customer = customerFacade.getCustomerByUserName(auth.getName(), store);
 
         }
-    	
-    	if(customer==null) {
-    		response.sendError(503, "Customer not logged in");
-    	}
-		
-		//get additional request parameters for orders
-		String lang = request.getParameter(Constants.LANG);		
-		String start = request.getParameter(Constants.START);
-		String max = request.getParameter(Constants.MAX);
-		
-		int startCount = 0;
-		int maxCount = 0;
-		
-		if(StringUtils.isBlank(lang)) {
-			lang = merchantStore.getDefaultLanguage().getCode();
-		}
-		
-		
-		Language language = languageService.getByCode(lang);
-		
-		if(language==null) {
-			LOGGER.error("Language is null for code " + lang);
-			response.sendError(503, "Language is null for code " + lang);
-			return null;
-		}
-		
-		try {
-			startCount = Integer.parseInt(start);
-		} catch (Exception e) {
-			LOGGER.info("Invalid value for start " + start);
-		}
-		
-		try {
-			maxCount = Integer.parseInt(max);
-		} catch (Exception e) {
-			LOGGER.info("Invalid value for max " + max);
-		}
-		
-		ReadableOrderList returnList = orderFacade.getReadableOrderList(merchantStore, customer, startCount, maxCount, language);
+        
 
-		return returnList;
-
-		
-	}
-	
-
-    
-    
-    @RequestMapping(value="/order.html", method={RequestMethod.GET,RequestMethod.POST})
-    public String order(final Model model,final HttpServletRequest request,@RequestParam(value = "orderId" ,required=true) final String orderId) throws Exception{
-        if(StringUtils.isBlank( orderId )){
-        	LOGGER.error( "Order Id can not be null or empty" );
-        }
-        LOGGER.info( "Fetching order details for Id " +orderId);
-        MerchantStore store = getSessionAttribute(Constants.MERCHANT_STORE, request);
         StringBuilder template = new StringBuilder().append(ControllerConstants.Tiles.Customer.customerOrder).append(".").append(store.getStoreTemplate());
         return template.toString();
         
